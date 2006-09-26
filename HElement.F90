30d981786f0b60ebaeee257f45c96ea9cd28165c
/C=GB/ST=Cambridgeshire/L=Cambridge/O=University of Cambridge/OU=Department of Chemistry/CN=ajwt3/emailAddress=ajwt3@cam.ac.uk </C=GB/ST=Cambridgeshire/L=Cambridge/O=University of Cambridge/OU=Department of Chemistry/CN=ajwt3/emailAddress=ajwt3@cam.ac.uk@3c9ecaa4-0e0d-0410-a8e4-acb18276dcea>
2006-09-26 16:35:34 +0000

    Added RhoElement and HElement codes missed in last update
    git-svn-id: https://wwmm.ch.cam.ac.uk/svn2/groups/alavi/NECI/trunk@3007 3c9ecaa4-0e0d-0410-a8e4-acb18276dcea

---
+++ HElement.F90
@@ -0,0 +1,308 @@
+MODULE HElement
+      IMPLICIT NONE
+      TYPE HElement
+#ifdef __CMPLX
+         COMPLEX*16  v
+#else
+         REAL*8  v
+#endif
+      END TYPE
+#ifdef __CMPLX
+      PARAMETER HElementSize=2
+#else
+      PARAMETER HElementSize=1
+#endif
+      interface operator (+)
+         module procedure HElemAdd
+      end interface
+      interface operator (-)
+         module procedure HElemSub
+         module procedure HElemNeg
+      end interface
+      interface operator (*)
+         module procedure HElemMul
+      end interface
+      interface operator (/)
+         module procedure HElemDiv
+         module procedure HElemDivInt
+      end interface
+      interface operator (.AGT.)
+         module procedure HElemAGT
+      end interface 
+      interface operator (.AGE.)
+         module procedure HElemAGE
+      end interface 
+      interface assignment (=)
+         module procedure HElemFromVal
+         module procedure HElemFromZVal
+         module procedure HElemFromHDElem
+      end interface
+      interface EXP
+         module procedure HElemExp
+      end interface
+      interface DCONJG
+         module procedure HElemDConjg
+      end interface
+      interface LOG
+         module procedure HElemLog
+      end interface
+      interface SQ
+         module procedure HElemSq
+      end interface
+
+      TYPE HDElement
+         REAL*8  v
+      END TYPE
+      PARAMETER HDElementSize=1
+      interface operator (+)
+         module procedure HDElemAdd
+      end interface
+      interface operator (-)
+         module procedure HDElemSub
+         module procedure HDElemNeg
+      end interface
+      interface operator (*)
+         module procedure HDElemMul
+      end interface
+      interface operator (/)
+         module procedure HDElemDiv
+         module procedure HDElemDivInt
+      end interface
+      interface operator (.AGT.)
+         module procedure HDElemAGT
+      end interface 
+      interface operator (.AGE.)
+         module procedure HDElemAGE
+      end interface 
+      interface assignment (=)
+         module procedure HDElemFromHElem
+         module procedure HDElemFromVal
+      end interface
+      interface DREAL
+         module procedure HDElemDReal
+      end interface
+      interface EXP
+         module procedure HDElemExp
+      end interface
+      interface LOG
+         module procedure HDElemLog
+      end interface
+      interface SQ
+         module procedure HDElemSq
+      end interface
+      CONTAINS
+
+!  We always initialize from reals
+      SUBROUTINE HElemFromVal(h,v)
+         TYPE(HElement) h
+         REAL*8  v
+         h%v=v
+         RETURN
+      END
+      SUBROUTINE HElemFromZVal(h,z)
+         TYPE(HElement) h
+         COMPLEX*16  z
+         h%v=z
+         RETURN
+      END
+      SUBROUTINE HElemFromHDElem(h,h2)
+         TYPE(HElement) h
+         TYPE(HDElement) h2
+         h%v=h2%v
+         RETURN
+      END
+      TYPE(HElement) FUNCTION HElemAdd(h1,h2)
+         TYPE(HElement) h1,h2
+         HElemAdd%v=h1%v+h2%v
+         RETURN
+      END
+      TYPE(HElement) FUNCTION HElemDConjg(h)
+         TYPE(HElement) h
+         HElemDConjg%v=DCONJG(h%v)
+         RETURN
+      END
+      TYPE(HElement) FUNCTION HElemSub(h1,h2)
+         TYPE(HElement) h1,h2
+         HElemSub%v=h1%v-h2%v
+         RETURN
+      END
+      TYPE(HElement) FUNCTION HElemNeg(h1)
+         TYPE(HElement) h1
+         HElemNeg%v=-h1%v
+         RETURN
+      END
+      TYPE(HElement) FUNCTION HElemMul(h1,h2)
+         TYPE(HElement) h1,h2
+         HElemMul%v=h1%v*h2%v
+         RETURN
+      END
+      TYPE(HElement) FUNCTION HElemDiv(h1,h2)
+         TYPE(HElement) h1,h2
+         HElemDiv%v=h1%v/h2%v
+         RETURN
+      END
+      TYPE(HElement) FUNCTION HElemDivInt(h1,i2)
+         TYPE(HElement) h1
+         INTEGER i2
+         HElemDivInt%v=h1%v/i2
+         RETURN
+      END
+
+      TYPE(HElement) FUNCTION HElemExp(h1)
+         TYPE(HElement) h1
+         HElemExp%v=EXP(h1%v)
+         RETURN
+      END
+      TYPE(HElement) FUNCTION HElemLog(h1)
+         TYPE(HElement) h1
+         HElemLog%v=LOG(h1%v)
+         RETURN
+      END
+      REAL*8 FUNCTION HElemSq(h1)
+         TYPE(HElement) h1
+         HElemSq=h1%v**2
+         RETURN
+      END
+
+      LOGICAL FUNCTION HElemAGT(h1,r2)
+         TYPE(HElement) h1
+         REAL*8 r2
+         HElemAGT=ABS(h1%v).GT.r2
+         RETURN
+      END
+      LOGICAL FUNCTION HElemAGE(h1,r2)
+         TYPE(HElement) h1
+         REAL*8 r2
+         HElemAGE=ABS(h1%v).GE.r2
+         RETURN
+      END
+      
+
+
+!.. GETHELEMENT2
+!.. Get matrix element of the hamiltonian
+!.. IC is the number of basis fns that differ in NI and NJ (or -1 if not known)
+!.. ECORE is the uniform background energy
+
+      TYPE(HElement) FUNCTION GetHElement2(NI,NJ,nEl,nBasisMax,G1,nBasis,Brr,NMSH,FCK,TMat,NMAX,ALAT,UMat,iC2,ECore)
+         INTEGER NMSH,NMAX
+         COMPLEX*16 FCK(*)
+         REAL*8 ALAT(*)
+         INCLUDE 'basis.inc'
+         TYPE(BasisFN) G1(*)
+         INTEGER NBASIS,BRR(*)
+         TYPE(HElement) TMat(*),UMat(*)
+         INTEGER I,nEl,NI(nEl),NJ(nEl),iC,nBasisMax(5,2),iC2
+         REAL*8 ECore
+         TYPE(HElement) Sum,Sum2
+         INTEGER IGETEXCITLEVEL
+         LOGICAL ISCSF
+         INTEGER ISUB
+         IF(ISCSF(NI,NEL).OR.ISCSF(NJ,NEL)) THEN
+            CALL CSFGETHELEMENT(NI,NJ,nEl,nBasisMax,G1,nBasis,Brr,NMSH,FCK,TMat,NMAX,ALAT,UMat,ECore,Sum2)
+            GETHELEMENT2=SUM2
+            RETURN
+         ENDIF
+         IC=IC2
+         GetHElement2%v=0.D0
+         IF(IC.LT.0) IC=IGETEXCITLEVEL(NI,NJ,NEL)
+!.. if we differ by more than 2 spin orbital, then the hamiltonian element is 0         
+         IF(IC.GT.2) RETURN
+!.. SLTCND has IC is # electrons the same in 2 dets
+         CALL TISET('GETHELEM2 ',ISUB)
+         CALL SltCnd(nEl,nBasisMax,nBasis,NI,NJ,G1,nEl-iC,NMSH,FCK,TMat,NMAX,ALAT,UMat,Sum)
+         GetHElement2=Sum
+         IF(iC.EQ.0) GetHElement2%v=GetHElement2%v+ECore
+         CALL TIHALT('GETHELEM2 ',ISUB)
+         RETURN
+      END
+
+!  We always initialize from reals
+       SUBROUTINE HDElemFromVal(h,v)
+         TYPE(HDElement) h
+         REAL*8  v
+         h%v=v
+         RETURN
+      END
+!.. Or HElements
+       SUBROUTINE HDElemFromHElem(h,h2)
+         TYPE(HDElement) h
+         TYPE(HElement) h2
+#ifdef __CMPLX
+         IF(DIMAG(h2%v).NE.0.D0) THEN
+            WRITE(6,*) "Conversion from complex to real:",h2%v
+            STOP "Imaginary part non-zero"
+         ENDIF
+         h%v=DREAL(h2%v)
+#else
+         h%v=h2%v
+#endif
+         RETURN
+      END
+      TYPE(HDElement) FUNCTION HDElemAdd(h1,h2)
+         TYPE(HDElement) h1,h2
+         HDElemAdd%v=h1%v+h2%v
+         RETURN
+      END
+      TYPE(HDElement) FUNCTION HDElemSub(h1,h2)
+         TYPE(HDElement) h1,h2
+         HDElemSub%v=h1%v-h2%v
+         RETURN
+      END
+      TYPE(HDElement) FUNCTION HDElemNeg(h1)
+         TYPE(HDElement) h1
+         HDElemNeg%v=-h1%v
+         RETURN
+      END
+      TYPE(HDElement) FUNCTION HDElemMul(h1,h2)
+         TYPE(HDElement) h1,h2
+         HDElemMul%v=h1%v*h2%v
+         RETURN
+      END
+      TYPE(HDElement) FUNCTION HDElemDiv(h1,h2)
+         TYPE(HDElement) h1,h2
+         HDElemDiv%v=h1%v/h2%v
+         RETURN
+      END
+      TYPE(HDElement) FUNCTION HDElemDivInt(h1,i2)
+         TYPE(HDElement) h1
+         INTEGER i2
+         HDElemDivInt%v=h1%v/i2
+         RETURN
+      END
+
+      TYPE(HDElement) FUNCTION HDElemExp(h1)
+         TYPE(HDElement) h1
+         HDElemExp%v=EXP(h1%v)
+         RETURN
+      END
+      TYPE(HDElement) FUNCTION HDElemLog(h1)
+         TYPE(HDElement) h1
+         HDElemLog%v=LOG(h1%v)
+         RETURN
+      END
+      REAL*8 FUNCTION HDElemSq(h1)
+         TYPE(HDElement) h1
+         HDElemSq=h1%v**2
+         RETURN
+      END
+
+      LOGICAL FUNCTION HDElemAGT(h1,r2)
+         TYPE(HDElement) h1
+         REAL*8 r2
+         HDElemAGT=ABS(h1%v).GT.r2
+         RETURN
+      END
+      LOGICAL FUNCTION HDElemAGE(h1,r2)
+         TYPE(HDElement) h1
+         REAL*8 r2
+         HDElemAGE=ABS(h1%v).GE.r2
+         RETURN
+      END
+      REAL*8 FUNCTION HDElemDReal(h)
+         TYPE(HDElement) h
+         HDElemDReal=h%v
+         RETURN
+      END
+END MODULE HElement
+
---
+++ RhoElement.F90
@@ -0,0 +1,127 @@
+!.. Calculate RHO_IJ using a Trotter decomposition without having a stored H
+!.. Calculate RHO = exp(-(BETA/P)H) matrix element <I|RHO|J> (= RHO_IJ)
+!.. Trotter = RHO ~ exp(-(BETA/P)H'/2)exp(-(BETA/P)U')exp(-(BETA/P)H'/2)
+!.. where H' is the diag part of H, and U' is the non-diag
+!.. 
+!.. NTAY is the order of the Taylor expansion for U'
+!.. IC is the number of basis fns by which NI and NJ differ (or -1 if not known)
+!.. 
+SUBROUTINE CALCRHO2(NI,NJ,BETA,I_P,NEL,NBASISMAX,G1,NBASIS,BRR,NMSH,FCK,TMat,&
+                     NMAX,ALAT,UMAT,RH,NTAY,IC2,ECORE)
+      USE HElement
+      TYPE(HElement) UMat(*),TMat(*),RH
+      INTEGER I_P,I_HMAX,NTAY,NTRUNC,NEL,NBASIS,NBASISMAX(5,2)
+      INTEGER NI(NEL),NJ(NEL),NMAX(3),IC,IC2
+      REAL*8 BETA
+      LOGICAL LSAME      
+      INTEGER NMSH,ISUB,I,BRR(NBASIS),J
+      INCLUDE 'basis.inc'
+      TYPE(BasisFN) G1(*)
+      COMPLEX*16 FCK(*)
+      REAL*8 ALAT(3)  
+      TYPE(HElement) hE,hE2,UExp,B
+      IF(NTAY.LT.0) THEN
+!.. We've actually hidden a matrix of rhos in the coeffs for calcing RHO
+         CALL GETRHOEXND(NI,NJ,NEL,BETA,NMSH,FCK,ZIA,UMAT,RH)
+         RETURN
+      ELSEIF(NTAY.EQ.0) THEN
+!.. NTAY=0 signifying we're going to calculate the RHO values when we
+!.. need them from the list of eigenvalues.
+!.. Hide NMSH=NEVAL
+!..      FCK=W
+!..      ZIA=CK
+!..      UMAT=NDET
+!..      ALAT=NMRKS
+          CALL CALCRHOEXND(NI,NJ,NEL,BETA,NMSH,FCK,TMat,UMAT,ALAT,NBASIS,I_P,ECORE,RH)
+         RETURN
+      ENDIF
+      CALL TISET('CALCRHO2  ',ISUB)
+      IC=IC2
+      B=BETA/I_P
+      UEXP=0.D0
+      IF(IC.LT.0)   IC=IGETEXCITLEVEL(NI,NJ,NEL)
+      IF(IC.EQ.0) THEN
+         LSAME=.TRUE.
+      ELSE
+         LSAME=.FALSE.
+      ENDIF
+          
+
+      IF(LSAME) THEN
+         UExp=UExp+HElement(1.D0)
+      ELSE
+!.. Now do the first order term, which only exists for non-diag
+         IF(NTAY.GE.1) THEN
+            UExp=UExp-B*GETHELEMENT2(NI,NJ,NEL,NBASISMAX,G1,&
+     &      NBASIS,BRR,NMSH,FCK,TMat,NMAX,ALAT,UMAT,IC,ECORE)
+         ENDIF
+      ENDIF
+!.. Now the 2nd order term
+      IF(NTAY.GE.2) UExp=UExp+B*B*HElement(RHO2ORDERND2(NI,NJ,NEL,NBASISMAX,    &
+     &            G1,NBASIS,BRR,NMSH,FCK,TMat,NMAX,ALAT,UMAT,IC,ECORE)/2.D0)
+
+      hE =(GETHELEMENT2(NI,NI,NEL,NBASISMAX,G1,NBASIS,BRR,NMSH,FCK,TMat,NMAX,ALAT,UMAT,0,ECORE) &
+          +GETHELEMENT2(NJ,NJ,NEL,NBASISMAX,G1,NBASIS,BRR,NMSH,FCK,TMat,NMAX,ALAT,UMAT,0,ECORE))&
+            /HElement(2.D0)
+      RH=EXP(HElement(-BETA/I_P)*hE)*UExp
+      CALL TIHALT('CALCRHO2  ',ISUB)
+      RETURN
+     END
+!.. RHO2ORDERND2(I,J,HAMIL,LAB,NROW,NDET
+!.. Calculate the 2nd order term in rho for non diag elements
+!.. IC2 is the number of basis fns that differ in NI and NJ (or -1 if not known)
+
+     FUNCTION Rho2OrderND2(NI,NJ,NEL,NBASISMAX,G1,NBASIS,BRR,NMSH,FCK,TMat,NMAX,ALAT,UMat,IC2,ECORE)
+!.. We use a crude method and generate all possible 0th, 1st, and 2nd
+!.. excitations of I and of J.  The intersection of these lists is the
+!.. selection of dets we want.
+         USE HElement
+         IMPLICIT NONE
+         INCLUDE 'basis.inc'
+         TYPE(BasisFN) G1(*)
+         TYPE(HElement) Rho2OrderND2
+         COMPLEX*16 FCK(*)
+         REAL*8 ALAT(*),ECORE
+         TYPE(HElement) UMat(*),TMat(*)
+         INTEGER NEL,NBASIS,NBASISMAX(5,2),BRR(*)
+         INTEGER NI(NEL),NJ(NEL),IC2,NMSH,NMAX
+         INTEGER LSTI(NEL,NBASIS*NBASIS*NEL*NEL)
+         INTEGER LSTJ(NEL,NBASIS*NBASIS*NEL*NEL)
+         INTEGER NLISTI, NLISTJ, IC,I,J,ICC
+         INTEGER ICI(NBASIS*NBASIS*NEL*NEL)
+         INTEGER ICJ(NBASIS*NBASIS*NEL*NEL),NLISTMAX
+         INTEGER CMP,IGETEXCITLEVEL,ICMPDETS
+         TYPE(HElement) SUM
+         SUM=0.D0
+         NLISTMAX=NBASIS*NBASIS*NEL*NEL
+         IC=IC2
+         IF(IC.LT.0) IC=IGETEXCITLEVEL(NI,NJ,NEL)
+
+!.. the 1 at the ends ensures K.NE. I or J, as this would make
+!.. <I|U'|K> or <K|U'|J> zero (U' has a zero diag)
+         CALL GENEXCIT(NI,2,NBASIS,NEL,LSTI,ICI,NLISTI,1,G1,.TRUE.,     &
+     &         NBASISMAX,.FALSE.)                                     
+         CALL GENEXCIT(NJ,2,NBASIS,NEL,LSTJ,ICJ,NLISTJ,1,G1,.TRUE.,     &
+     &         NBASISMAX,.FALSE.)
+         I=1
+         J=1
+!.. Now iterate over K, going along row I
+         DO WHILE ((I.LE.NLISTI).AND.(J.LE.NLISTJ))
+            CMP=ICMPDETS(LSTI(1,I),LSTJ(1,J),NEL)
+!.. While I>J, we increase J
+            DO WHILE ((CMP.GT.0).AND.(J.LT.NLISTJ))
+               J=J+1
+               CMP=ICMPDETS(LSTI(1,I),LSTJ(1,J),NEL)
+            ENDDO
+            IF(CMP.EQ.0) THEN 
+               SUM=SUM+  GETHELEMENT2(NI,LSTI(1,I),NEL,NBASISMAX,       &
+     &      G1,NBASIS,BRR,NMSH,FCK,TMat,NMAX,ALAT,UMat,ICI(I),ECORE)     &
+     &               *GETHELEMENT2(LSTJ(1,J),NJ,NEL,NBASISMAX,          &
+     &      G1,NBASIS,BRR,NMSH,FCK,TMat,NMAX,ALAT,UMat,ICJ(J),ECORE)
+            ENDIF
+            I=I+1
+         
+         ENDDO
+         RHO2ORDERND2=SUM
+         RETURN
+      END
