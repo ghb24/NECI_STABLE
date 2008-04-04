f83b5c8d7e2965473f2191d62c2c89a01eb619ae
/C=GB/ST=Cambridgeshire/L=Cambridge/O=University of Cambridge/OU=Department of Chemistry/CN=ghb24/emailAddress=ghb24@cam.ac.uk </C=GB/ST=Cambridgeshire/L=Cambridge/O=University of Cambridge/OU=Department of Chemistry/CN=ghb24/emailAddress=ghb24@cam.ac.uk@3c9ecaa4-0e0d-0410-a8e4-acb18276dcea>
2008-04-04 10:45:51 +0000

    Changed the 'abs' to '.agt.' should now compile with complex orbitals...
    git-svn-id: https://wwmm.ch.cam.ac.uk/svn2/groups/alavi/NECI/trunk@8930 3c9ecaa4-0e0d-0410-a8e4-acb18276dcea

--- GraphMorph.F90
+++ GraphMorph.F90
@@ -482,7 +482,7 @@ MODULE GraphMorph
                     connected=.false.
                     do j=1,NDets
                         IF(i.eq.j) CYCLE
-                        IF(abs(Rhoij(i-1,j-1)%v).gt.0.D0) THEN
+                        IF(Rhoij(i-1,j-1).agt.0.D0) THEN
                             connected=.true.
                             EXIT
                         ENDIF
@@ -527,7 +527,7 @@ MODULE GraphMorph
                     connected=.false.
                     do j=1,NDets
                         IF(i.eq.j) CYCLE
-                        IF(abs(Rhoij(i-1,j-1)%v).gt.0.D0) THEN
+                        IF(Rhoij(i-1,j-1).agt.0.D0) THEN
                             connected=.true.
                             EXIT
                         ENDIF
