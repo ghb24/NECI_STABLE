39ca9fd1c4027ee13f67e54fecbf4d2e28563efb
jss43 <jss43@3c9ecaa4-0e0d-0410-a8e4-acb18276dcea>
2008-07-21 22:32:16 +0000

    Interface routines to vasp. Initialisation routines for vasp-based calculations.
    git-svn-id: https://wwmm.ch.cam.ac.uk/svn2/groups/alavi/NECI/trunk@10221 3c9ecaa4-0e0d-0410-a8e4-acb18276dcea

---
+++ vasp_init.F90
@@ -0,0 +1,133 @@
+subroutine VaspSystemInit(ArrLEN)
+   use System, only: Symmetry,SymmetrySize,SymmetrySizeB
+   use System, only: BasisFN,BasisFNSize,BasisFNSizeB
+   use vasp_interface
+   implicit none
+   include 'sym.inc'
+   integer :: ArrLEN
+   integer :: i,ik
+
+   ArrLEN=nStates*2
+   nRot=nKP
+   PropBitLen=15
+   nProp(:)=KPMsh(:)
+   tAbelian=.true.
+   write (6,*) tAbelian,nProp,PropBitLen,nRot,ArrLEN
+
+   call Memory(IP_KPntSym,3*nKP,'KPntSym')
+   do ik=1,nKP
+      do i=1,3
+         KPntSym(i,ik)=kpnts(i,ik)*nProp(i)
+      end do
+   end do
+
+   write (6,*) tAbelian,nProp,PropBitLen,nRot,ArrLEN
+   return
+end subroutine VaspSystemInit
+
+subroutine VASPInitIntegrals(nOrbUsed,ECore,tOrder)
+   use HElem
+   use System, only: BasisFN,nEl
+   use OneEInts, only: TMatSym, TMatInd
+   use vasp_interface
+   use UMatCache, only: SetupUMatCache,UMat2D
+   use MemoryManager, only: LogMemAlloc
+   implicit none
+   integer :: nOrbUsed
+   real(q) ::  ECore
+   logical :: tOrder
+   integer :: iSub,I,J,II,A,B,nStatesUsed,ierr
+   type(HElement) :: HarXC,HarXCSum
+   character(*), parameter :: thisroutine='VASPInitIntegrals'
+   
+   call TISET('VASPInitInts',ISUB)
+   ! ECore=EIonIon???
+   ECore=0.d0
+   write (6,*) 'Core Energy: ',ECORE
+   nStatesUsed=nOrbUsed/2
+
+   call SetupUMatCache(nStatesUsed,NSTATESUSED.NE.NSTATES)
+
+   HarXCSum=dcmplx(0.d0,0.d0)
+   write (6,*) "Calculating TMAT"
+   do I=1,nStatesUsed
+      ! Subtract out the double counting. Assume closed-shell.
+      HarXC=dcmplx(0.d0,0.d0)
+      do J=1,nEl/2
+         if (I.ne.J) then
+            A=min(I,J)
+            B=max(I,J)
+            HarXC=HarXC-HElement(2)*UMat2D(A,B)+UMat2D(B,A)
+         end if
+         if (i.le.j) HarXCSum=HarXCSum+HarXC
+      end do
+      II=I*2-1
+      TMATSYM(TMatInd(II+1,II+1))=HElement(eigv(I))-HarXC
+      write (6,*) II+1,TMATSYM(TMatInd(II+1,II+1))
+      write (6,*) I,HarXCSum
+   end do
+   
+
+   if (tOrder) then
+      call StopGM('VASPInitIntegrals','tOrder not implemented in VASP interface yet.')
+   end if
+   WRITE (6,*) "Finished TMAT"
+   CLOSE (10)
+
+   call TIHALT('VASPInitInts',ISUB)
+   
+   return
+end subroutine VASPInitIntegrals
+
+subroutine VASPBasisInit(ARR,BRR,G1,LEN)
+   ! Largely lifted from the CPMD analogue.  Should be doing (roughly) the same
+   ! thing to get going!
+   use System, only: Symmetry,SymmetrySize,SymmetrySizeB
+   use System, only: BasisFN,BasisFNSize,BasisFNSizeB,nBASISMax
+   use vasp_interface, only: q,nStates,nKP,KPntInd,eigv
+   implicit none
+   include 'sym.inc'
+   real(q) :: ARR(LEN,2)
+   integer :: BRR(LEN),LEN
+   type(BasisFN) :: G1(LEN)
+   integer :: i
+   type(Symmetry) :: iDecomp
+   integer(8) :: ComposeAbelianSym
+
+   NBASISMAX(1:3,1:2)=0
+   NBASISMAX(1,3)=2
+   NBASISMAX(4,1)=-1
+   NBASISMAX(4,2)=1
+
+!  set ISPINSKIP=0, to tell the SCRs that there's no UMAT
+   NBASISMAX(2,3)=0
+
+   call GenKPtIrreps(NKP,NKP,KPNTIND,NSTATES)
+
+   call IAZZERO(G1,LEN*BasisFNSize)
+
+   do i=1,nStates
+      IDECOMP%s=ComposeAbelianSym(KpntSym(:,KPntInd(I)))
+      G1(I*2-1)%SYM=IDECOMP
+      G1(I*2)%SYM=IDECOMP
+      G1(I*2-1)%MS=-1
+      G1(I*2)%MS=1
+      G1(I*2-1)%K(1:3)=0
+      G1(I*2)%K(1:3)=0
+      ARR(2*I-1,1)=eigv(I)
+      ARR(2*I,1)=eigv(I)
+      ARR(2*I-1,2)=eigv(I)
+      ARR(2*I,2)=eigv(I)
+      BRR(2*I-1)=2*I-1
+      BRR(2*I)=2*I
+   end do
+
+   WRITE (6,*) 'Using Abelian symmetry formulation.'
+   NBASISMAX(5,1)=0
+   NBASISMAX(5,2)=NSYM-1
+
+! Show it's a generic spatial basis
+   NBASISMAX(3,3)=1
+
+   return
+end subroutine VASPBasisInit
---
+++ vasp_interface.F90
@@ -0,0 +1,58 @@
+module vasp_interface
+
+implicit none
+public
+save
+integer, parameter :: q=kind(0.d0)
+
+integer, allocatable :: KPntInd(:)
+real(q), allocatable :: eigv(:),kpnts(:,:)
+real(q) :: xi
+
+integer :: nKP,nStates,KPMsh(3)
+
+contains
+
+   subroutine NECIReceiveVASPData(vasp_nbands,vasp_nkpts,vasp_kpnts,vasp_kpmsh,vasp_xi,vasp_kpntind,vasp_eigv,vasp_umat2D)
+      use HElem
+      use UMatCache, only: SetupUMatCache,UMat2D,tagUMat2D,tUMat2D
+      implicit none
+      integer :: vasp_nbands,vasp_nkpts,vasp_kpntind(vasp_nbands*vasp_nkpts),vasp_kpmsh(3)
+      real(q) :: vasp_kpnts(3,vasp_nkpts),vasp_xi,vasp_eigv(vasp_nbands*vasp_nkpts)
+      complex(q) :: vasp_umat2d(vasp_nbands*vasp_nkpts,vasp_nbands*vasp_nkpts)
+      integer :: i,j,ik,ierr
+
+      nKP=vasp_nkpts
+      nStates=vasp_nbands*vasp_nkpts
+
+      write (6,*) 'nkp',nKP
+      write (6,*) 'kp'
+      do i=1,nKP
+         write (6,*) vasp_kpnts(:,i)
+      end do 
+
+      allocate(KPNTInd(nStates))
+      allocate(eigv(nStates))
+      allocate(kpnts(3,nKP))
+
+      KPMsh(:)=vasp_kpmsh(:)
+      KPNTInd(:)=vasp_kpntind(:)
+      eigv(:)=vasp_eigv(:)
+      kpnts(:,:)=vasp_kpnts(:,:)
+      xi=vasp_xi
+
+      ! Set up UMat2D
+      tUMat2D=.TRUE.
+      Allocate(UMat2D(nStates,nStates),STAT=ierr)
+      Call MemAlloc(ierr,UMat2D,HElementSize*NSTATES*NSTATES,'UMAT2D')
+!      call LogMemAlloc('UMat2D',nStates**2,HElementSize*8,thisroutine,tagUMat2D,ierr)
+      do i=1,nStates
+         do j=1,nStates
+            UMat2D(i,j)=HElement(vasp_umat2d(i,j))
+         end do
+      end do
+      
+      return
+   end subroutine NECIReceiveVASPData
+
+end module vasp_interface
