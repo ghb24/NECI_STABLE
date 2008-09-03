e4c2b357ae1fdaa7e9da78d3ea1a2f80065d7c34
jss43 <jss43@3c9ecaa4-0e0d-0410-a8e4-acb18276dcea>
2008-09-03 19:32:31 +0000

    Further documentation of the soft_exit module.
    git-svn-id: https://wwmm.ch.cam.ac.uk/svn2/groups/alavi/NECI/trunk@11120 3c9ecaa4-0e0d-0410-a8e4-acb18276dcea

--- soft_exit.F90
+++ soft_exit.F90
@@ -9,23 +9,29 @@ module soft_exit
 != input file have been performed.
 
 != 1. At the start of a calculation, create the file SOFTEXIT::
-!=      call init_soft_exit()
+!=        call init_soft_exit()
 != 2. During the calculation, test for the existence of the file SOFTEXIT.  If
 !=    test_SOFTEXIT is false, then SOFTEXIT has been removed and the
 !=    programmer's code should finish up loose ends (e.g. and post-processing of data
 !=    required) and exit cleanly.
+!=    If you wish to exit immediately, rather than trapping the value returned by
+!=    test_SOFTEXIT and then tracing back through the calling stack and hence avoid
+!=    any further unwanted calculations, use test_SOFTEXIT with the termination
+!=    routines provided in NECICore.F90 and in error_handling.F90::
+!=        if (.not.test_SOFTEXIT) then
+!=            call NECICalcEnd(0)
+!=            call NECICodeEnd()
+!=            call quiet_stop()
+!=        end if
 != 3. At the end of the calculation, if SOFTEXIT is still around, we remove it
-!=    because it's polite to tidy up after ourselves.      
+!=    because it's polite to tidy up after ourselves::
+!=        call end_soft_exit()
 
 != To do:
 !=  * Parallel version.  The file SOFTEXIT is created on all nodes.  However,
 !=    test_SOFTEXIT should be false if SOFTEXIT is removed from *any* node.
 !=    This involves MPI communication (and JSS doesn't know all the MPI routines by
 !=    heart yet).
-!=  * Helper exit function: rather than trapping the value returned by
-!=    test_SOFTEXIT and then tracing back through the calling stack and avoiding
-!=    and unwanted calculations, provide a routine which, if test_SOFTEXIT is
-!=    false, will just exit cleanly for you. 
 
 implicit none
 
