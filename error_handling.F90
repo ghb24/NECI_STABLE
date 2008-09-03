be583b3581c31be767ae9ed8f4456bd8470a28c6
jss43 <jss43@3c9ecaa4-0e0d-0410-a8e4-acb18276dcea>
2008-09-03 19:31:05 +0000

    Add a routine to perform a quiet exit with no error messages (or clean up, etc): just halt everything.
    git-svn-id: https://wwmm.ch.cam.ac.uk/svn2/groups/alavi/NECI/trunk@11114 3c9ecaa4-0e0d-0410-a8e4-acb18276dcea

--- error_handling.F90
+++ error_handling.F90
@@ -10,7 +10,7 @@ subroutine stop_all(sub_name,error_msg)
 use Parallel, only: iProcIndex,MPIStopAll
 #endif
 implicit none
-character(*) :: sub_name,error_msg
+character(*), intent(in) :: sub_name,error_msg
 
 ! It seems that giving STOP a string is far more portable.
 ! MPI_Abort requires an integer though.
@@ -43,10 +43,34 @@ subroutine warning(sub_name,error_msg)
 !=    error_msg: error message.
 
 implicit none
-character(*) :: sub_name,error_msg
+character(*), intent(in) :: sub_name,error_msg
 
 write (6,'(/a)') 'WARNING.  Error in '//adjustl(sub_name)
 write (6,'(a/)') adjustl(error_msg)
 
 return
 end subroutine warning
+
+
+
+subroutine quiet_stop(msg)
+!= Exit without making any noise.  Useful for when there's no error, but you
+!= still want to exit midway through a calculation (e.g. for testing purposes,
+!= or for use with the SOFTEXIT functionality).
+!= In:
+!=    msg (optional) : Print msg before exiting if msg is present.
+
+implicit none
+character(*), intent(in), optional :: msg
+
+if (present(msg)) then
+    write (6,'(X,a)') adjustl(msg)
+end if
+
+#ifdef PARALLEL
+call MPIStopAll(error_str)
+#else
+stop
+#endif
+
+end subroutine quiet_stop
