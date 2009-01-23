23055eeda708f933bbc0a0b6e9db3f95b5e6a321
ghb24 <ghb24@3c9ecaa4-0e0d-0410-a8e4-acb18276dcea>
2009-01-23 15:24:36 +0000

    First draft transformation scheme added.
    git-svn-id: https://wwmm.ch.cam.ac.uk/svn2/groups/alavi/NECI/trunk@13774 3c9ecaa4-0e0d-0410-a8e4-acb18276dcea

--- LocalizeOrbs.F90
+++ LocalizeOrbs.F90
@@ -65,22 +65,83 @@ MODULE LocalizeOrbsMod
     SUBROUTINE Transform2EInts()
 
 !Zero arrays from previous transform
+!<alpha beta | gamma delta> integrals are found from UMAT(UMatInd(i,j,k,l,0,0)
 
-!loop over i
-    !loop over beta,gamma,delta
-        !sum over alpha to store <i beta | gamma delta> - OneIndInts
+!loop over i's in each symmetry irrep
+        do i=1,SpatOrbs
+!loop over beta,gamma,delta
+            do b=1,SpatOrbs
+                do g=1,SpatOrbs
+                    do d=1,b    !delta =< beta
+!If statement here - the <i b | g d> must be symmetry allowed...
+                        t=0.D0
+                        do a=1,SpatOrbs     !This loop only wants to be over the atomic orbitals in the symmetry class of the current i loop
+!sum over alpha to store <i beta | gamma delta> - OneIndInts
+                            t=t+Coeff(i,a)*UMAT(UMatInd(a,b,g,d,0,0))
+                        enddo
+                        OneIndInts(i,b,g,d)=t
+                        OneIndInts(i,d,g,b)=t
+                    enddo
+                enddo
+            enddo
+        enddo
 
-!loop over j
-    !loop over i, gamma, delta
-        !sum over beta to store <i j | gamma delta> - TwoIndInts
+        do k=1,SpatOrbs !loop over k orbitals in each symmetry irrep
+            do i=1,k    !i =< k
+                do b=1,SpatOrbs
+                    do d=1,b    !d =< b
+                        !Can test that (i,k) < (b,d) here
+                        !Can test that <i b | k d> is symmetry allowed here
+                        t=0.D0
+!sum over gamma to store <i beta | j delta> - TwoIndInts
+                        do g=1,SpatOrbs
+                            t=t+Coeff(k,g)*OneIndInts(i,b,g,d)
+                        enddo
+                        TwoIndInts(i,b,k,d)=t
+                        TwoIndInts(k,b,i,d)=t
+                        TwoIndInts(i,d,k,b)=t
+                        TwoIndInts(k,d,i,b)=t
+                    enddo
+                enddo
+            enddo
+        enddo
 
-!loop over k
-    !loop over i, j, delta
-        !sum over gamma to store <i j | k delta> - ThreeIndInts
+        do j=1,SpatOrbs !loop over j orbitals in each symmetry irrep
+            do i=1,SpatOrbs
+                do k=1,i    !k =< i
+                    do d=1,SpatOrbs
+                        !Test is symmetry allowed here
+                        t=0.D0
+!sum over beta to store <i j | k delta> - ThreeIndInts
+                        do b=1,SpatOrbs
+                            t=t+Coeff(j,b)*TwoIndInts(i,b,k,d)
+                        enddo
+                        ThreeIndInts(i,j,k,d)=t
+                        ThreeIndInts(k,j,i,d)=t
+                    enddo
+                enddo
+            enddo
+        enddo
 
-!loop over l
-    !loop over i,j,k
-        !sum over delta to store <i j | k l> - FourIndInts
+        do l=1,SpatOrbs !loop over l orbitals in each symmetry irrep
+            do j=1,l    ! j =< l
+                do i=1,SpatOrbs
+                    do k=1,i    ! k =< i
+                        !Test that (i,k) =< (j,l) here
+                        !Test that integral is symmetry allowed
+                        t=0.D0
+!sum over delta to store <i j | k l> - FourIndInts
+                        do d=1,SpatOrbs
+                            t=t+Coeff(l,d)*ThreeIndInts(i,j,k,d)
+                        enddo
+                        FourIndInts(i,j,k,l)=t
+                        FourIndInts(k,j,i,l)=t
+                        FourIndInts(i,l,k,j)=t
+                        FourIndInts(k,l,i,j)=t
+                    enddo
+                enddo
+            enddo
+        enddo
 
     END SUBROUTINE Transform2EInts
 
