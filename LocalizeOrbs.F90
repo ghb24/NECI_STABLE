2767a77c52d1daa2d9ea30295c05dba32334bd3c
ghb24 <ghb24@3c9ecaa4-0e0d-0410-a8e4-acb18276dcea>
2009-01-23 16:32:05 +0000

    More added to localize orbs routines.
    git-svn-id: https://wwmm.ch.cam.ac.uk/svn2/groups/alavi/NECI/trunk@13775 3c9ecaa4-0e0d-0410-a8e4-acb18276dcea

--- LocalizeOrbs.F90
+++ LocalizeOrbs.F90
@@ -4,11 +4,12 @@ MODULE LocalizeOrbsMod
     REAL*8 , ALLOCATABLE :: Lambdas(:,:)
     REAL*8 , ALLOCATABLE :: DerivCoeff(:,:)
     REAL*8 , ALLOCATABLE :: DerivLambda(:,:)
-    REAL*8 , ALLOCATABLE :: OneIndInts(:,:), TwoIndInts(:,:), ThreeIndInts(:,:), FourIndInts(:,:)   !These are the arrays to store the
-                                                                                                    !partially transformed integrals for
-                                                                                                    !each iteration. The FourIndInts are
-                                                                                                    !the full <ij|kl> integrals for the
-                                                                                                    !current iteration.
+    REAL*8 , ALLOCATABLE :: OneIndInts(:,:,:,:), TwoIndInts(:,:,:,:), ThreeIndInts(:,:,:,:), FourIndInts(:,:,:,:)   !These are the arrays to store the
+!partially transformed integrals for each iteration. The FourIndInts are the full <ij|kl> integrals for the current iteration.
+!OneIndInts = <i b|g d> ; TwoIndInts = <i b|k d> ; ThreeIndInts = <i j|k d>
+    INTEGER :: SpatOrbs,OneIndIntsTag,TwoIndIntsTag,ThreeIndIntsTag,FourIndIntsTag
+    INTEGER :: CoeffTag,LambdasTag,DerivCoeffTag,DerivLambdaTag
+    LOGICAL :: tNotConverged
 
     contains
 
@@ -28,7 +29,9 @@ MODULE LocalizeOrbsMod
 
 !option to create new FCIDUMP?
 
-        CALL DeallocAllMem()
+        CALL DeallocateMem()
+
+        CLOSE(12)
 
     END SUBROUTINE LocalizeOrbs
 
@@ -62,9 +65,18 @@ MODULE LocalizeOrbsMod
 
     END SUBROUTINE FindTheForce
 
+    
+!This is an M^5 transform, which transforms all the two-electron integrals into the new basis described by the Coeff matrix.
+!This is v memory inefficient and currently does not use any spatial symmetry information.
     SUBROUTINE Transform2EInts()
+        INTEGER :: i,j,k,l,a,b,g,d
+        REAL*8 :: t
 
 !Zero arrays from previous transform
+        OneIndInts(:,:,:,:)=0.D0
+        TwoIndInts(:,:,:,:)=0.D0
+        ThreeIndInts(:,:,:,:)=0.D0
+        FourIndInts(:,:,:,:)=0.D0
 !<alpha beta | gamma delta> integrals are found from UMAT(UMatInd(i,j,k,l,0,0)
 
 !loop over i's in each symmetry irrep
@@ -144,6 +156,98 @@ MODULE LocalizeOrbsMod
         enddo
 
     END SUBROUTINE Transform2EInts
+
+
+    SUBROUTINE InitLocalOrbs()
+        USE SystemData , only : nBasis
+        CHARACTER(len=*) , PARAMETER :: this_routine='InitLocalOrbs'
+        REAL*8 :: t
+        INTEGER :: i,j,k,l,ierr
+
+        SpatOrbs=nBasis/2
+
+!Allocate memory
+        ALLOCATE(Coeff(SpatOrbs,SpatOrbs),stat=ierr)
+        CALL LogMemAlloc('Coeff',SpatOrbs**2,8,this_routine,CoeffTag,ierr)
+        ALLOCATE(Lambdas(SpatOrbs,SpatOrbs),stat=ierr)
+        CALL LogMemAlloc('Lambdas',SpatOrbs**2,8,this_routine,LambdasTag,ierr)
+        ALLOCATE(DerivCoeff(SpatOrbs,SpatOrbs),stat=ierr)
+        CALL LogMemAlloc('DerivCoeff',SpatOrbs**2,8,this_routine,DerivCoeffTag,ierr)
+        ALLOCATE(DerivLambda(SpatOrbs,SpatOrbs),stat=ierr)
+        CALL LogMemAlloc('DerivLambda',SpatOrbs**2,8,this_routine,DerivLambdaTag,ierr)
+        ALLOCATE(OneIndInts(SpatOrbs,SpatOrbs,SpatOrbs,SpatOrbs),stat=ierr)
+        CALL LogMemAlloc('OneIndInts',SpatOrbs**4,8,this_routine,OneIndIntsTag,ierr)
+        ALLOCATE(TwoIndInts(SpatOrbs,SpatOrbs,SpatOrbs,SpatOrbs),stat=ierr)
+        CALL LogMemAlloc('TwoIndInts',SpatOrbs**4,8,this_routine,TwoIndIntsTag,ierr)
+        ALLOCATE(ThreeIndInts(SpatOrbs,SpatOrbs,SpatOrbs,SpatOrbs),stat=ierr)
+        CALL LogMemAlloc('ThreeIndInts',SpatOrbs**4,8,this_routine,ThreeIndIntsTag,ierr)
+        ALLOCATE(FourIndInts(SpatOrbs,SpatOrbs,SpatOrbs,SpatOrbs),stat=ierr)
+        CALL LogMemAlloc('FourIndInts',SpatOrbs**4,8,this_routine,FourIndIntsTag,ierr)
+
+!Zero/initialise the arrays
+        Coeff(:,:)=0.D0
+        do i=1,SpatOrbs
+            Coeff(i,i)=1.D0
+        enddo
+        Lambdas(:,:)=0.D0
+        DerivCoeff(:,:)=0.D0
+        DerivLambda(:,:)=0.D0
+!These loops can be speeded up with spatial symmetry and pairwise permutation symmetry if needed.
+!Fill all the original partially transformed integral arrays with the original two-electron integrals.
+        do i=1,SpatOrbs
+            do k=1,i
+                do j=1,SpatOrbs
+                    do l=1,j
+                        t=UMAT(UMatInd(i,j,k,l,0,0))
+                        FourIndInts(i,j,k,l)=t
+                        FourIndInts(k,j,i,l)=t
+                        FourIndInts(i,l,k,j)=t
+                        FourIndInts(k,l,i,j)=t
+                        ThreeIndInts(i,j,k,l)=t
+                        ThreeIndInts(k,j,i,l)=t
+                        ThreeIndInts(i,l,k,j)=t
+                        ThreeIndInts(k,l,i,j)=t
+                        TwoIndInts(i,j,k,l)=t
+                        TwoIndInts(k,j,i,l)=t
+                        TwoIndInts(i,l,k,j)=t
+                        TwoIndInts(k,l,i,j)=t
+                        OneIndInts(i,j,k,l)=t
+                        OneIndInts(k,j,i,l)=t
+                        OneIndInts(i,l,k,j)=t
+                        OneIndInts(k,l,i,j)=t
+                    enddo
+                enddo
+            enddo
+        enddo
+
+        OPEN(12,FILE='Transform.Dat',STATUS='unknown')
+
+    END SUBROUTINE InitLocalOrbs
+
+    SUBROUTINE DeallocateMem()
+        CHARACTER(len=*) , PARAMETER :: this_routine='DeallocateMem'
+
+        DEALLOCATE(Coeff)
+        CALL LogMemDealloc(this_routine,CoeffTag)
+        DEALLOCATE(Lambdas)
+        CALL LogMemDealloc(this_routine,LambdasTag)
+        DEALLOCATE(DerivCoeff)
+        CALL LogMemDealloc(this_routine,DerivCoeffTag)
+        DEALLOCATE(DerivLambda)
+        CALL LogMemDealloc(this_routine,DerivLambdaTag)
+        DEALLOCATE(OneIndInts)
+        CALL LogMemDealloc(this_routine,OneIndIntsTag)
+        DEALLOCATE(TwoIndInts)
+        CALL LogMemDealloc(this_routine,TwoIndIntsTag)
+        DEALLOCATE(ThreeIndInts)
+        CALL LogMemDealloc(this_routine,ThreeIndIntsTag)
+        DEALLOCATE(FourIndInts)
+        CALL LogMemDealloc(this_routine,FourIndIntsTag)
+
+    END SUBROUTINE DeallocateMem
+        
+
+
 
         
 
