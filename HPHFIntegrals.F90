62bc5a2da1755f0d9b9b04de4f5d3e456d2fd603
ghb24 <ghb24@3c9ecaa4-0e0d-0410-a8e4-acb18276dcea>
2009-07-02 16:34:29 +0000

    Another speedup for the HPHF stuff. No redundant off-diagonal matrix elements now calculated. Hoping that the diagonal matrix elements can be similarly speeded up.
    git-svn-id: https://wwmm.ch.cam.ac.uk/svn2/groups/alavi/NECI/trunk@16946 3c9ecaa4-0e0d-0410-a8e4-acb18276dcea

--- HPHFIntegrals.F90
+++ HPHFIntegrals.F90
@@ -12,7 +12,7 @@ SUBROUTINE HPHFGetOffDiagHElement(nI,nJ,MatEl)
     TYPE(HElement) :: MatEl,MatEl2
     LOGICAL :: TestClosedShellDet,tSymmetricInts,DetBitEQ
 
-    MatEl=HElement(0.D0)
+    MatEl%v=0.D0
 
     CALL EncodeBitDet(nI,iLutnI,NEl,NIfD)
     CALL EncodeBitDet(nJ,iLutnJ,NEl,NIfD)
@@ -28,12 +28,7 @@ SUBROUTINE HPHFGetOffDiagHElement(nI,nJ,MatEl)
 !            CALL FLUSH(6)
             CALL FindBitExcitLevel(iLutnI,iLutnJ,NIfD,ExcitLevel,2)
             IF(ExcitLevel.le.2) THEN
-                IF(ExcitLevel.le.0) THEN
-                    WRITE(6,*) iLutnI(:),iLutnJ(:),ExcitLevel
-                    WRITE(6,*) "***",nI(:)
-                    WRITE(6,*) "***",nJ(:)
-                    CALL Stop_All("HPHFGetOffDiagHElement","Determinants are a forbidden excitation level apart9")
-                ENDIF
+!                IF(ExcitLevel.le.0) CALL Stop_All("HPHFGetOffDiagHElement","Determinants are a forbidden excitation level apart9")
                 CALL SltCnd(nEl,nBasisMax,nBasis,nI,nJ,G1,nEl-ExcitLevel,NMSH,FCK,NMAX,ALAT,UMat,MatEl)
             ENDIF
             RETURN
@@ -41,71 +36,96 @@ SUBROUTINE HPHFGetOffDiagHElement(nI,nJ,MatEl)
 !Closed Shell -> Open Shell. <X|H|Y>= 1/sqrt(2) [Hia + Hib], or with a minus if iLutnJ has an odd number of open orbitals.
 !            WRITE(6,*) "Closed Shell -> Open Shell"
 !            CALL FLUSH(6)
-            CALL CalcOpenOrbs(iLutnJ,NIfD,NEl,OpenOrbsJ)
-
-            CALL FindDetSpinSym(nJ,nJ2,NEl)
-            CALL FindExcitBitDetSym(iLutnJ,iLutnJ2)
-
 !First, find <nI|H|nJ>
             CALL FindBitExcitLevel(iLutnI,iLutnJ,NIfD,ExcitLevel,2)
             IF(ExcitLevel.le.2) THEN
-                IF(ExcitLevel.le.0) CALL Stop_All("HPHFGetOffDiagHElement","Determinants are a forbidden excitation level apart8")
-                MatEl2=HElement(0.D0)
+                MatEl2%v=0.D0
                 CALL SltCnd(NEl,nBasisMax,nBasis,nI,nJ,G1,NEl-ExcitLevel,NMSH,FCK,NMAX,ALAT,UMat,MatEl2)
-                MatEl=MatEl+MatEl2
+                MatEl%v=SQRT(2.D0)*MatEl2%v
             ENDIF
-!Now, find <nI|H|nJ2>
-            CALL FindBitExcitLevel(iLutnI,iLutnJ2,NIfD,ExcitLevel,2)
-            IF(ExcitLevel.le.2) THEN
-                IF(ExcitLevel.le.0) CALL Stop_All("HPHFGetOffDiagHElement","Determinants are a forbidden excitation level apart7")
-                MatEl2=HElement(0.D0)
-                CALL SltCnd(NEl,nBasisMax,nBasis,nI,nJ2,G1,NEl-ExcitLevel,NMSH,FCK,NMAX,ALAT,UMat,MatEl2)
-                IF(mod(OpenOrbsJ,2).eq.0) THEN
-!Open-shell HPHF is a symmetric combination - therefore, add contribution.
-                    MatEl=MatEl+MatEl2
-                ELSE
-                    MatEl=MatEl-MatEl2
-                ENDIF
-            ENDIF
-            MatEl%v=MatEl%v/SQRT(2.D0)
+
+
+!Below is the old way of doing this, however, the integrals are the same (for the same reason as os -> cs)
+!            CALL CalcOpenOrbs(iLutnJ,NIfD,NEl,OpenOrbsJ)
+!
+!            CALL FindDetSpinSym(nJ,nJ2,NEl)
+!            CALL FindExcitBitDetSym(iLutnJ,iLutnJ2)
+!
+!!First, find <nI|H|nJ>
+!            CALL FindBitExcitLevel(iLutnI,iLutnJ,NIfD,ExcitLevel,2)
+!            IF(ExcitLevel.le.2) THEN
+!                IF(ExcitLevel.le.0) CALL Stop_All("HPHFGetOffDiagHElement","Determinants are a forbidden excitation level apart8")
+!                MatEl2=HElement(0.D0)
+!                CALL SltCnd(NEl,nBasisMax,nBasis,nI,nJ,G1,NEl-ExcitLevel,NMSH,FCK,NMAX,ALAT,UMat,MatEl2)
+!                MatEl=MatEl+MatEl2
+!                WRITE(6,*) 1,MatEl2%v,ExcitLevel
+!            ENDIF
+!!Now, find <nI|H|nJ2>
+!            CALL FindBitExcitLevel(iLutnI,iLutnJ2,NIfD,ExcitLevel,2)
+!            IF(ExcitLevel.le.2) THEN
+!                IF(ExcitLevel.le.0) CALL Stop_All("HPHFGetOffDiagHElement","Determinants are a forbidden excitation level apart7")
+!                MatEl2=HElement(0.D0)
+!                CALL SltCnd(NEl,nBasisMax,nBasis,nI,nJ2,G1,NEl-ExcitLevel,NMSH,FCK,NMAX,ALAT,UMat,MatEl2)
+!                IF(mod(OpenOrbsJ,2).eq.0) THEN
+!!Open-shell HPHF is a symmetric combination - therefore, add contribution.
+!                    MatEl=MatEl+MatEl2
+!                    WRITE(6,*) 2,MatEl2%v,ExcitLevel
+!                ELSE
+!                    MatEl=MatEl-MatEl2
+!                    WRITE(6,*) 2,-1.D0*MatEl2%v,ExcitLevel
+!                ENDIF
+!            ENDIF
+!            MatEl%v=MatEl%v/SQRT(2.D0)
             RETURN
         ENDIF
     ELSE
-!Initial HPHF is open-shell. Find the spin-coupled determinant.
-        CALL FindDetSpinSym(nI,nI2,NEl)
-        CALL FindExcitBitDetSym(iLutnI,iLutnI2)
-
-!Original HPHF is antisymmetric if OpenOrbs is odd, or symmetric if its even.
-        CALL CalcOpenOrbs(iLutnI,NIfD,NEl,OpenOrbsI)
-
         IF(TestClosedShellDet(iLutnJ,NIfD)) THEN
 !            WRITE(6,*) "Open Shell -> Closed Shell"
 !            CALL FLUSH(6)
+!            WRITE(6,*) "***"
 !OpenShell -> Closed Shell. I am pretty sure that if one of the determinants is connected, then the other is connected with the same IC (+matrix element?) Test this later.
             CALL FindBitExcitLevel(iLutnI,iLutnJ,NIfD,ExcitLevel,2)
             IF(ExcitLevel.le.2) THEN
-                IF(ExcitLevel.le.0) CALL Stop_All("HPHFGetOffDiagHElement","Determinants are a forbidden excitation level apart6")
-                MatEl2=HElement(0.D0)
+!                IF(ExcitLevel.le.0) CALL Stop_All("HPHFGetOffDiagHElement","Determinants are a forbidden excitation level apart6")
+                MatEl2%v=0.D0
                 CALL SltCnd(NEl,nBasisMax,nBasis,nI,nJ,G1,NEl-ExcitLevel,NMSH,FCK,NMAX,ALAT,UMat,MatEl2)
-                MatEl=MatEl+MatEl2
+!                WRITE(6,*) 1,MatEl2%v,Excitlevel
+!                WRITE(6,*) 2,MatEl2%v,Excitlevel
+                MatEl%v=MatEl%v+SQRT(2.D0)*MatEl2%v
             ENDIF
-            CALL FindBitExcitLevel(iLutnI2,ilutnJ,NIfD,ExcitLevel,2)
-            IF(ExcitLevel.le.2) THEN
-                IF(ExcitLevel.le.0) CALL Stop_All("HPHFGetOffDiagHElement","Determinants are a forbidden excitation level apart5")
-                MatEl2=HElement(0.D0)
-                CALL SltCnd(NEl,nBasisMax,nBasis,nI2,nJ,G1,NEl-ExcitLevel,NMSH,FCK,NMAX,ALAT,UMat,MatEl2)
-                IF(mod(OpenOrbsI,2).eq.0) THEN
-!Open-shell HPHF is a symmetric combination - add integral contribution.
-                    MatEl=MatEl+MatEl2
-                ELSE
-                    MatEl=MatEl-MatEl2
-                ENDIF
-            ENDIF
-            MatEl%v=MatEl%v/SQRT(2.D0)
+
+
+!This is the old way of doing it, however, both integrals have the same absolute value, and the sign from the sym/anti sym cancels the sign change of the two determinants.
+!            CALL FindBitExcitLevel(iLutnI,iLutnJ,NIfD,ExcitLevel,2)
+!            IF(ExcitLevel.le.2) THEN
+!                IF(ExcitLevel.le.0) CALL Stop_All("HPHFGetOffDiagHElement","Determinants are a forbidden excitation level apart6")
+!                MatEl2=HElement(0.D0)
+!                CALL SltCnd(NEl,nBasisMax,nBasis,nI,nJ,G1,NEl-ExcitLevel,NMSH,FCK,NMAX,ALAT,UMat,MatEl2)
+!                WRITE(6,*) 1,MatEl2%v,Excitlevel
+!                MatEl=MatEl+MatEl2
+!            ENDIF
+!            CALL FindBitExcitLevel(iLutnI2,ilutnJ,NIfD,ExcitLevel,2)
+!            IF(ExcitLevel.le.2) THEN
+!                IF(ExcitLevel.le.0) CALL Stop_All("HPHFGetOffDiagHElement","Determinants are a forbidden excitation level apart5")
+!                MatEl2=HElement(0.D0)
+!                CALL SltCnd(NEl,nBasisMax,nBasis,nI2,nJ,G1,NEl-ExcitLevel,NMSH,FCK,NMAX,ALAT,UMat,MatEl2)
+!                IF(mod(OpenOrbsI,2).eq.0) THEN
+!!Open-shell HPHF is a symmetric combination - add integral contribution.
+!                    MatEl=MatEl+MatEl2
+!                    WRITE(6,*) 2,MatEl2%v,ExcitLevel
+!                ELSE
+!                    MatEl=MatEl-MatEl2
+!                    WRITE(6,*) 2,-MatEl2%v,ExcitLevel
+!                ENDIF
+!            ENDIF
+!            MatEl%v=MatEl%v/SQRT(2.D0)
             RETURN
 
         ELSE
 !OpenShell -> Open Shell. Find the spin pair of nJ.
+!Initial HPHF is open-shell. Find the spin-coupled determinant.
+            CALL FindExcitBitDetSym(iLutnI,iLutnI2)
+
 !            WRITE(6,*) "Open Shell -> Open Shell"
 !            CALL FLUSH(6)
 !            WRITE(6,*) "***"
@@ -116,7 +136,7 @@ SUBROUTINE HPHFGetOffDiagHElement(nI,nJ,MatEl)
 !                    WRITE(6,*) ExcitLevel,iLutnI,iLutnJ
 !                    CALL Stop_All("HPHFGetOffDiagHElement","Determinants are a forbidden excitation level apart4")
 !                ENDIF
-                MatEl2=HElement(0.D0)
+                MatEl2%v=0.D0
                 CALL SltCnd(NEl,nBasisMax,nBasis,nI,nJ,G1,NEl-ExcitLevel,NMSH,FCK,NMAX,ALAT,UMat,MatEl2)
 !                WRITE(6,*) 1,REAL(MatEl2%v,8),ExcitLevel
 !                WRITE(6,*) 4,REAL(MatEl2%v,8),ExcitLevel
@@ -127,14 +147,18 @@ SUBROUTINE HPHFGetOffDiagHElement(nI,nJ,MatEl)
             CALL FindBitExcitLevel(iLutnI2,ilutnJ,NIfD,ExcitLevel,2)
             IF(ExcitLevel.le.2) THEN
 !We need to find out whether the nJ HPHF wavefunction is symmetric or antisymmetric. This is dependant on the number of open shell orbitals.
+                CALL FindDetSpinSym(nI,nI2,NEl)
                 CALL CalcOpenOrbs(iLutnJ,NIfD,NEl,OpenOrbsJ)
+!Original HPHF is antisymmetric if OpenOrbs is odd, or symmetric if its even.
+                CALL CalcOpenOrbs(iLutnI,NIfD,NEl,OpenOrbsI)
+
                 IF((mod(OpenOrbsI,2).eq.0).and.(mod(OpenOrbsJ,2).eq.0)) THEN
                     tSymmetricInts=.true.
                 ELSE
                     tSymmetricInts=.false.
                 ENDIF
 !                IF(ExcitLevel.le.0) CALL Stop_All("HPHFGetOffDiagHElement","Determinants are a forbidden excitation level apart3")
-                MatEl2=HElement(0.D0)
+                MatEl2%v=0.D0
                 CALL SltCnd(NEl,nBasisMax,nBasis,nI2,nJ,G1,NEl-ExcitLevel,NMSH,FCK,NMAX,ALAT,UMat,MatEl2)
                 IF(tSymmetricInts.or.((mod(OpenOrbsI,2).eq.0).and.(mod(OpenOrbsJ,2).eq.1))) THEN
 !                    WRITE(6,*) 2,REAL(MatEl2%v,8),ExcitLevel
