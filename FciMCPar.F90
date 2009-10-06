902f8f0210b1a49876eb0e3532d68c55d51779d4
ajwt3 <ajwt3@3c9ecaa4-0e0d-0410-a8e4-acb18276dcea>
2009-10-06 10:11:36 +0000

    EQUILSTEPS was not correctly working with the shift averaging.
    git-svn-id: https://wwmm.ch.cam.ac.uk/svn2/groups/alavi/NECI/trunk@18367 3c9ecaa4-0e0d-0410-a8e4-acb18276dcea

--- FciMCPar.F90
+++ FciMCPar.F90
@@ -5534,7 +5534,7 @@ MODULE FciMCParMod
         IF(iProcIndex.eq.Root) THEN
             IF(.not.TSinglePartPhase) THEN
                 DiagSft=DiagSft-(log(AllGrowRate)*SftDamp)/(Tau*(StepsSft+0.D0))
-                IF((Iter-VaryShiftIter).gt.(NEquilSteps*StepsSft)) THEN
+                IF((Iter-VaryShiftIter).gt.NEquilSteps) THEN
 !                    WRITE(6,*) Iter-VaryShiftIter, NEquilSteps*StepsSft
                     VaryShiftCycles=VaryShiftCycles+1
                     SumDiagSft=SumDiagSft+DiagSft
