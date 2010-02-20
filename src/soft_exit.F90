34ec3a7c3240def9b36dafcce3cc4f142b6c4d31
James Spencer <jss43@cam.ac.uk>
2010-02-20 16:16:23 +0000

    Step 1 in tidying soft_exit.
    Essentially I removed code duplication.
    
    Remove unused functions now that ChangeVars exists.
    
    Turn test_SoftExit into a wrapper around ChangeVars.

--- src/soft_exit.F90
+++ src/soft_exit.F90
@@ -1,28 +1,28 @@
 module soft_exit
 
-! Based almost entirely on work by GHB.
+! JSS, based almost entirely on work by GHB.
 
-!= During the calculation, test for the existence of the file SOFTEXIT.  If
-!= test_SOFTEXIT is true, then SOFTEXIT has been created (by, we hope, the
-!= user) and the programmer's code should finish up loose ends (e.g. and 
-!= post-processing of data required) and exit cleanly.
-!=
-!= The existence of this file can be tested via the function TestSoftExit,
-!= which allows a clean exit to be performed, e.g. to enable termination
-!= of a MC calculation before the number of steps specified in the input
-!= file have been performed.
-!=
-!= If you wish to exit immediately, rather than trapping the value returned by
-!= test_SOFTEXIT and then tracing back through the calling stack and hence avoid
-!= any further unwanted calculations, use test_SOFTEXIT with the termination
-!= routines provided in NECICore.F90 and in error_handling.F90::
-!=     if (test_SOFTEXIT) then
-!=         call NECICalcEnd(0)
-!=         call NECICodeEnd(tCPMD,tVASP)
-!=         call quiet_stop()
-!=     end if
-!= Alternatively, you can call the SOFTEXIT_exit subroutine, which does exactly
-!= the above.
+! During the calculation, test for the existence of the file SOFTEXIT.  If
+! test_SOFTEXIT is true, then SOFTEXIT has been created (by, we hope, the
+! user) and the programmer's code should finish up loose ends (e.g. and 
+! post-processing of data required) and exit cleanly.
+!
+! The existence of this file can be tested via the function TestSoftExit,
+! which allows a clean exit to be performed, e.g. to enable termination
+! of a MC calculation before the number of steps specified in the input
+! file have been performed.
+!
+! If you wish to exit immediately, rather than trapping the value returned by
+! test_SOFTEXIT and then tracing back through the calling stack and hence avoid
+! any further unwanted calculations, use test_SOFTEXIT with the termination
+! routines provided in NECICore.F90 and in error_handling.F90::
+!     if (test_SOFTEXIT) then
+!         call NECICalcEnd(0)
+!         call NECICodeEnd(tCPMD,tVASP)
+!         call quiet_stop()
+!     end if
+! Alternatively, you can call the SOFTEXIT_exit subroutine, which does exactly
+! the above.
 
 implicit none
 
@@ -423,122 +423,6 @@ contains
     
     end subroutine ChangeVars
 
-
-    logical function test_SOFTEXIT()
-       != Test if the file SOFTEXIT exists.
-       != Return True means that the file is there and you should start to exit cleanly.
-       != False means that the calculation should proceed as normal. 
-
-       use Parallel
-       implicit none
-       logical :: exists
-       logical :: AnyExist,deleted_file,any_deleted_file
-       integer :: error,ierr,i
-
-       inquire(file='SOFTEXIT',exist=exists)
-       !This collective will check the exists logical on all nodes, and perform a logical or operation,
-       !before broadcasting the result back to all nodes.
-       CALL MPI_AllReduce(exists,AnyExist,1,MPI_LOGICAL,MPI_LOR,MPI_COMM_WORLD,error)
-       test_SOFTEXIT=AnyExist
-       if(test_SOFTEXIT) then
-           write (6,'(X,a)') 'Request for SOFTEXIT detected on a node.'
-           deleted_file=.false.
-           do i=0,nProcessors-1
-               ! This causes each processor to attempt to delete
-               ! SOFTEXIT in turn (as each cycle of the loop involves waiting
-               ! for all processors to reach the AllReduce before the next cycle 
-               ! can start, and hence avoid race conditions between processors 
-               ! sharing the same disk.
-               if (i==iProcIndex.and.exists) then
-                   open(13,file='SOFTEXIT')
-                   close(13,status='delete')
-                   deleted_file=.true.
-               end if
-               call MPI_AllReduce(deleted_file,any_deleted_file,1,MPI_LOGICAL,MPI_LOR,MPI_COMM_WORLD,error)
-               if (any_deleted_file) exit
-           end do
-       endif
-
-    end function test_SOFTEXIT
-    
-    
-    logical function test_ExpandSpace(iLevel)
-       != Test if the file EXPANDSPACE exists.
-       != If it does, it reads the file to find out the excitation level to which the space should be expanded to.
-       != Return True means that the file is there and you should start to expand the space to the value iLevel which is returned.
-       != False means that the calculation should proceed as normal. 
-
-       use Parallel
-       implicit none
-       logical :: exists
-       logical :: AnyExist,deleted_file,any_deleted_file
-       integer :: error,ierr,i,iLevel
-
-       inquire(file='EXPANDSPACE',exist=exists)
-       !This collective will check the exists logical on all nodes, and perform a logical or operation,
-       !before broadcasting the result back to all nodes.
-       CALL MPI_AllReduce(exists,AnyExist,1,MPI_LOGICAL,MPI_LOR,MPI_COMM_WORLD,error)
-       test_ExpandSpace=AnyExist
-       if(test_ExpandSpace) then
-           deleted_file=.false.
-           do i=0,nProcessors-1
-               ! This causes each processor to attempt to delete
-               ! SOFTEXIT in turn (as each cycle of the loop involves waiting
-               ! for all processors to reach the AllReduce before the next cycle 
-               ! can start, and hence avoid race conditions between processors 
-               ! sharing the same disk.
-               if (i==iProcIndex.and.exists) then
-                   open(13,file='EXPANDSPACE')
-                   READ(13,*) iLevel
-                   close(13,status='delete')
-                   deleted_file=.true.
-               end if
-               call MPI_AllReduce(deleted_file,any_deleted_file,1,MPI_LOGICAL,MPI_LOR,MPI_COMM_WORLD,error)
-               if (any_deleted_file) exit
-           end do
-           call MPI_BCast(iLevel,1,MPI_INTEGER,i,MPI_COMM_WORLD,error)
-       endif
-
-    end function test_ExpandSpace
-    
-    
-    logical function test_VaryShift()
-       != Test if the file VARYSHIFT exists.
-       != Return True means that the file is there and you should start to vary the shift.
-       != False means that the calculation should proceed as normal. 
-
-       use Parallel
-       implicit none
-       logical :: exists
-       logical :: AnyExist,deleted_file,any_deleted_file
-       integer :: error,ierr,i
-
-       inquire(file='VARYSHIFT',exist=exists)
-       !This collective will check the exists logical on all nodes, and perform a logical or operation,
-       !before broadcasting the result back to all nodes.
-       CALL MPI_AllReduce(exists,AnyExist,1,MPI_LOGICAL,MPI_LOR,MPI_COMM_WORLD,error)
-       test_VaryShift=AnyExist
-       if(test_VaryShift) then
-           write (6,'(X,a)') 'Request to vary the shift detected on a node.'
-           deleted_file=.false.
-           do i=0,nProcessors-1
-               ! This causes each processor to attempt to delete
-               ! SOFTEXIT in turn (as each cycle of the loop involves waiting
-               ! for all processors to reach the AllReduce before the next cycle 
-               ! can start, and hence avoid race conditions between processors 
-               ! sharing the same disk.
-               if (i==iProcIndex.and.exists) then
-                   open(13,file='VARYSHIFT')
-                   close(13,status='delete')
-                   deleted_file=.true.
-               end if
-               call MPI_AllReduce(deleted_file,any_deleted_file,1,MPI_LOGICAL,MPI_LOR,MPI_COMM_WORLD,error)
-               if (any_deleted_file) exit
-           end do
-       endif
-
-    end function test_VaryShift
-    
 #else
     
     subroutine ChangeVars(tSingBiasChange,tSoftExitFound,tWritePopsFound)
@@ -888,87 +772,16 @@ contains
        END IF
     
     end subroutine ChangeVars
-    
-    logical function test_SOFTEXIT()
-       != Test if the file SOFTEXIT exists.
-       != Return True means that the file is there and you should start to exit cleanly.
-       != False means that the calculation should proceed as normal. 
 
-       implicit none
-       logical :: exists
-
-       inquire(file='SOFTEXIT',exist=exists)
-       test_SOFTEXIT=exists
-       ! We'll also do some house-keeping whilst we're here.
-       if (test_SOFTEXIT) then
-           ! Remove it so it doesn't catch us out the next time the calculation
-           ! is run!
-           open(13,file='SOFTEXIT')
-           close(13,status='delete')
-           write (6,'(X,a)') 'Request for SOFTEXIT detected.'
-       end if
-
-    end function test_SOFTEXIT
-    
-    logical function test_VaryShift()
-       != Test if the file VARYSHIFT exists.
-       != Return True means that the file is there and you should start to vary the shift.
-       != False means that the calculation should proceed as normal. 
-
-       implicit none
-       logical :: exists
-
-       inquire(file='VARYSHIFT',exist=exists)
-       test_VaryShift=exists
-       ! We'll also do some house-keeping whilst we're here.
-       if (test_VaryShift) then
-           ! Remove it so it doesn't catch us out the next time the calculation
-           ! is run!
-           open(13,file='VARYSHIFT')
-           close(13,status='delete')
-           write (6,'(X,a)') 'Request to vary the shift detected.'
-       end if
-
-    end function test_VaryShift
-    
-    logical function test_ExpandSpace(iLevel)
-       != Test if the file ExpandSpace exists.
-       != Return True means that the file is there and you should start to expandspace.
-       != False means that the calculation should proceed as normal. 
-
-       implicit none
-       logical :: exists
-       integer :: iLevel
-
-       inquire(file='EXPANDSPACE',exist=exists)
-       test_ExpandSpace=exists
-       ! We'll also do some house-keeping whilst we're here.
-       if (test_ExpandSpace) then
-           ! Remove it so it doesn't catch us out the next time the calculation
-           ! is run!
-           open(13,file='EXPANDSPACE')
-           READ(13,*) iLevel
-           close(13,status='delete')
-       end if
-
-    end function test_ExpandSpace
-    
 #endif
-    
-    subroutine SOFTEXIT_exit()
-       != If SOFTEXIT has been created, then exit cleanly and immediately, with
-       != no further calculations.
-       != If SOFTEXIT has not been created, do nothing.
-    
-        use SystemData, only: tCPMD,tVASP
-        implicit none
 
-        if (test_SOFTEXIT()) then
-            call NECICalcEnd(0)
-            call NECICodeEnd(tCPMD,tVASP)
-            call quiet_stop()
-        end if
+logical function test_SoftExit()
 
-    end subroutine SOFTEXIT_exit
+    logical :: tdummy1, tdummy2
+
+    call ChangeVars(tdummy1, test_SoftExit, tdummy2)
+    if (test_SoftExit) write (6,'(1X,a30)') 'Request for SOFTEXIT detected.'
+
+end function test_SoftExit
 
 end module soft_exit
