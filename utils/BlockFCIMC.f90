17cba9ec8cc3b8b7db2e90468435aab91a640b8b
George Booth <ghb24@bari.ch.cam.ac.uk>
2010-03-05 14:21:57 +0000

    Bug fixes in blocking program

--- utils/BlockFCIMC.f90
+++ utils/BlockFCIMC.f90
@@ -5,12 +5,15 @@ PROGRAM BlkFCIMC
       INTEGER :: i,Iter,WalkCng,TotWalkers,Annihil,Died,Born,StartIter,ierr,TotPoints
       INTEGER :: TotBlkSize,NoBlocks,BlockSize,VecInd,Blk,j
       REAL*8 :: Sft,GrowRate,ProjE,AvBlock,SumBlock,Mean,MeanSq,SD,Error,ErrorinError,AvShift,ProjEInst
-      REAL*8 , ALLOCATABLE :: Shifts(:),Energies(:),BlkAv(:)
+      REAL*8 :: AvBlockEn,SumBlockEn,MeanEn,MeanSqEn,SDEn,ErrorEn,ErrorinErrorEn
+      REAL*8 , ALLOCATABLE :: Shifts(:),Energies(:),BlkAv(:),BlkAvEn(:)
 
       OPEN(UNIT=11,FILE='FCIMCStats',STATUS='OLD',ACTION='READ',    &
         POSITION='REWIND')
       OPEN(UNIT=12,FILE='BlockingInfo',STATUS='UNKNOWN')
-      WRITE(12,*) '#  Blocklength   Log_2 BL     MeanBlocks              SD-Blocks               Err-Blocks           Err in Err-Blocks'
+      OPEN(UNIT=13,FILE='BlockingInfoEnergies',STATUS='UNKNOWN')
+      WRITE(12,"(A)") '#  Blocklength   Log_2 BL     MeanBlocks              SD-Blocks               Err-Blocks           Err in Err-Blocks'
+      WRITE(13,"(A)") '#  Blocklength   Log_2 BL     MeanBlocks              SD-Blocks               Err-Blocks           Err in Err-Blocks'
 
       READ(11,*) FirstLineRubbish
     
@@ -60,10 +63,12 @@ PROGRAM BlkFCIMC
 
 !Allocate data to hold averages of each block
       ALLOCATE(BlkAv(TotPoints),stat=ierr)
+      ALLOCATE(BlkAvEn(TotPoints),stat=ierr)
       IF(ierr.ne.0) THEN
           STOP 'Error in allocation'
       ENDIF
       BlkAv(:)=0.D0
+      BlkAvEn(:)=0.D0
 
 !      do i=1,TotPoints
 !          WRITE(7,'(I12,G16.7)') 500,Shifts(i)
@@ -85,13 +90,16 @@ PROGRAM BlkFCIMC
           NoBlocks=0            !This is the counter for the number of blocks with a given blocksize
           VecInd=1              !Start at the beginning of the list of data to create the blocks
           BlkAv(:)=0.D0         !Rezero the block averages
+          BlkAvEn(:)=0.D0         !Rezero the block averages
 
           do while(VecInd.le.TotPoints)  !Carry on looping over the data until we have 
 
               Blk=0             !This is the size of the block we are growing
               SumBlock=0.D0     !This is the sum of the elements in the block
+              SumBlockEn=0.D0     !This is the sum of the elements in the block
               do while(Blk.lt.BlockSize)
                   SumBlock=SumBlock+Shifts(VecInd)
+                  SumBlockEn=SumBlockEn+Energies(VecInd)
                   Blk=Blk+1
                   VecInd=VecInd+1
                   IF(VecInd.gt.TotPoints) EXIT      !Break out of the loop when we reach the end of the list
@@ -101,8 +109,10 @@ PROGRAM BlkFCIMC
               IF(Blk.eq.Blocksize) THEN
 !Block is complete - find the average, and move onto the next block of the data
                   AvBlock=SumBlock/REAL(BlockSize)
+                  AvBlockEn=SumBlockEn/REAL(BlockSize)
                   NoBlocks=NoBlocks+1
                   BlkAv(NoBlocks)=AvBlock      !Store the average of each block
+                  BlkAvEn(NoBlocks)=AvBlockEn      !Store the average of each block
               ELSE
 !We have reached the end of the file - chuck the last few data points for this blocksize
                   IF(VecInd.ne.(TotPoints+1)) THEN
@@ -119,24 +129,37 @@ PROGRAM BlkFCIMC
 
 !First, find the mean & sd of all blocks...
           Mean=0.D0     
+          MeanEn=0.D0     
           do j=1,NoBlocks
               Mean=Mean+BlkAv(j)
               MeanSq=MeanSq+(BlkAv(j)**2)
+              MeanEn=MeanEn+BlkAvEn(j)
+              MeanSqEn=MeanSqEn+(BlkAvEn(j)**2)
           enddo
           Mean=Mean/REAL(NoBlocks)
           MeanSq=MeanSq/REAL(NoBlocks)
+          MeanEn=MeanEn/REAL(NoBlocks)
+          MeanSqEn=MeanSqEn/REAL(NoBlocks)
 
           SD=SQRT(MeanSq-(Mean**2))
           Error=SD/SQRT(REAL(NoBlocks-1))
 
+          SDEn=SQRT(MeanSqEn-(MeanEn**2))
+          ErrorEn=SDEn/SQRT(REAL(NoBlocks-1))
+
 !This is from the blocking paper, and indicates the error in the blocking error, due to the limited number of blocks available.
           ErrorinError=Error/SQRT(2.D0*(REAL(NoBlocks-1)))
 
+          ErrorinErrorEn=ErrorEn/SQRT(2.D0*(REAL(NoBlocks-1)))
+
 !Write out info
           WRITE(12,'(2I12,4G25.16)') BlockSize,i,Mean,SD,Error,ErrorinError
+          
+          WRITE(13,'(2I12,4G25.16)') BlockSize,i,MeanEn,SDEn,ErrorEn,ErrorinErrorEn
 
       enddo
       CLOSE(12)
+      CLOSE(13)
 
 
 END PROGRAM BlkFCIMC
