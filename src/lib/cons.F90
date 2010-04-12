2407a143dd69343fde202621c56ada5074b5b069
Simon Smart <sds50@obex.ch.cam.ac.uk>
2010-04-12 11:28:21 +0100

    Added support for 64bit integers to util_mod_cpts.F90.template
    --> Also added integer kinds int32 and int64 to constants
    --> Added some (incomplete) int64 support to quicksort.F90.template

--- src/lib/cons.F90
+++ src/lib/cons.F90
@@ -3,6 +3,10 @@ module constants
 ! Constant data.
 
 integer, parameter :: dp = selected_real_kind(15,307)
+integer, parameter :: int32 = selected_int_kind(8)
+integer, parameter :: int64 = selected_int_kind(18)
+
+
 
 real(dp), parameter ::  PI    = 3.1415926535897932384626433832795028841971693993751_dp
 real(dp), parameter ::  PI2   = 9.8696044010893586188344909998761511353136994072408_dp
