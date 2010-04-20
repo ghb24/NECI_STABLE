0fa1786f9a170be14476f30fc1e64eee5b577c17
George Booth <ghb24@bari.ch.cam.ac.uk>
2010-04-20 16:22:00 +0100

    merged the new n_int c-preprocessing parameter

--- src/lib/cons.F90
+++ src/lib/cons.F90
@@ -8,4 +8,19 @@ real(dp), parameter ::  PI    = 3.1415926535897932384626433832795028841971693993
 real(dp), parameter ::  PI2   = 9.8696044010893586188344909998761511353136994072408_dp
 real(dp), parameter ::  THIRD = 0.3333333333333333333333333333333333333333333333333_dp
 
+integer, parameter :: sizeof_int = 4
+integer, parameter :: sizeof_dp = 8
+integer, parameter :: sizeof_sp = 4
+#ifdef __CMPLX
+integer, parameter :: sizeof_helement = 16
+#else
+integer, parameter :: sizeof_helement = 8
+#endif
+
+#ifdef __INT64
+integer, parameter :: n_int=int64
+#else
+integer, parameter :: n_int=int32
+#endif
+
 end module constants
