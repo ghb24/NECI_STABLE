15b4ff04502b4d3df157f4379fd48cb5026c377b
Simon Smart <sds50@obex.ch.cam.ac.uk>
2010-04-27 11:22:18 +0100

    I am an idiot

--- src/fcimc_wrapper.F90
+++ src/fcimc_wrapper.F90
@@ -14,11 +14,11 @@ subroutine assign_proc (ptr, proc)
     !      compiler to crash on valid code.
 #ifdef __PATHSCALE__
 #ifdef __PTR64
-    type(int64), intent(in), value :: proc
-    type(int64), intent(inout) :: ptr
+    integer(int64), intent(in), value :: proc
+    integer(int64), intent(inout) :: ptr
 #else
-    type(int32), intent(in), value :: proc
-    type(int32), intent(inout) :: ptr
+    integer(int32), intent(in), value :: proc
+    integer(int32), intent(inout) :: ptr
 #endif
 #else
     type(c_ptr), intent(in), value :: proc
