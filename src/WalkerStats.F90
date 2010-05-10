c7fe599b07c504252292dce3acfe24ca585d2052
George Booth <ghb24@bari.ch.cam.ac.uk>
2010-05-10 14:08:09 +0100

    Initial create of module

---
+++ src/WalkerStats.F90
@@ -0,0 +1,17 @@
+!This module provides routines for extracting and inputting walker properties to the combined 
+!walker array.
+
+MODULE WalkerStatsMod
+
+      IMPLICIT NONE
+      save
+
+      contains
+
+      SUBROUTINE SetupBitPositions
+
+      RETURN
+
+      END SUBROUTINE SetupBitPositions
+
+END MODULE WalkerStatsMod
