f034c7d81d3542931c3bde825c748d20fe5e5bab
Simon Smart <sds50@obex.ch.cam.ac.uk>
2010-05-11 18:27:26 +0100

    Re-removed data definitions from BitReps.F90 --> bit_rep_data.F90

--- src/BitReps.F90
+++ src/BitReps.F90
@@ -19,23 +19,6 @@ module bit_reps
     ! (1         * 32-bits if needed)   Signs (Im)
     ! (1         * 32-bits if needed)   Flags
 
-    integer :: nIfTot  ! Upper bound of bit representation. In form 0:NIfTot
-    integer :: nIfD    ! Final byte representing spatial/spin orbitals
-
-    integer :: nOffY   ! Offset of Yamanouchi symbol. n.b. always on a 64bit
-                       ! boundary --> This is in 64-bit integers.
-                       ! TODO: ensure Yamanouchi symbols are 32bit.
-    integer :: nIfY    ! Number of bytes to represent a Yamanouchi symbol
-
-    integer :: nIfDBO  ! Size used for bit operations (e.g. sorting) - this is 
-                       ! the determinant (+ Yamanouchi if needed)
-
-    integer :: nOffFlag   ! Offset of flags. in bytes
-    integer :: nIfFlag    ! Number of bytes to contain flags.
-
-    integer :: nOffSgn  ! Offset of signs in integers
-    integer :: nIfSgn   ! Number of integers used for signs
-
 contains
 
     subroutine allocate_currentdets ()
