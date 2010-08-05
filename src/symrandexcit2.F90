332b9e44a066af227d1892e14716b275479a87a4
Simon Smart <sds50@obex.ch.cam.ac.uk>
2010-08-05 19:34:20 +0100

    Removed non-conforming tabs

--- src/symrandexcit2.F90
+++ src/symrandexcit2.F90
@@ -1514,13 +1514,13 @@ MODULE GenRandSymExcitNUMod
                 SymB=0
             ELSE
 !Calculate the symmetry product of the occupied orbital pair
-				ElecSym=RandExcitSymLabelProd(SpinOrbSymLabel(OrbI),SpinOrbSymLabel(OrbJ))
+                ElecSym=RandExcitSymLabelProd(SpinOrbSymLabel(OrbI),SpinOrbSymLabel(OrbJ))
 !                ElecSym=INT(IEOR(G1(OrbI)%Sym%S,G1(OrbJ)%Sym%S),4)
 !This will calculate the A orbitals which will have no B pair
                 CALL FindNumForbiddenOrbs(ForbiddenOrbs,ClassCountUnocc2,ElecSym,iSpn,SumMl)
 !Need to find the symmetries of the unoccupied A and B orbitals.
-				SymA=SpinOrbSymLabel(OrbA)
-				SymB=RandExcitSymLabelProd(SymInvLabel(SymA),ElecSym)
+                SymA=SpinOrbSymLabel(OrbA)
+                SymB=RandExcitSymLabelProd(SymInvLabel(SymA),ElecSym)
 !                SymA=INT(G1(OrbA)%Sym%S,4)
 !                SymB=IEOR(SymA,ElecSym)
             ENDIF
