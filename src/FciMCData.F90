299368cfad5dea1ec7d355dbc55e8b3da2b73193
Simon Smart <sds50@obex.ch.cam.ac.uk>
2010-08-10 11:15:11 +0100

    A bit of tweaking
    --> root is now declared in the module Parallel

--- src/FciMCData.F90
+++ src/FciMCData.F90
@@ -7,8 +7,6 @@ MODULE FciMCData
       implicit none
       save
 
-      INTEGER , PARAMETER :: Root=0   !This is the rank of the root processor
-
       ! Units used to write to files
       integer :: fcimcstats_unit ! FCIMCStats
       integer :: initiatorstats_unit ! INITIATORStats
