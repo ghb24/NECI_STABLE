cb466f07db275b15d77d12a78500353516404b99
James Shepherd <js615@cam.ac.uk>
2011-03-16 01:25:14 +0000

    Fix to gcutoff keyword - now direct/exchange MP2 terms are treated the same

--- src/FciMCPar.F90
+++ src/FciMCPar.F90
@@ -5767,7 +5767,7 @@ MODULE FciMCParMod
         integer :: Ki(3),Kj(3),Ka(3),LowLoop,HighLoop,X,i,Elec1Ind,Elec2Ind,K,Orbi,Orbj
         integer :: iSpn,FirstA,nJ(NEl),a,Ex(2,2),kx,ky,kz,OrbB,FirstB
         logical :: tParity,tMom
-        real*8 :: Ranger,mp2,mp2all,length,length_g
+        real*8 :: Ranger,mp2,mp2all,length,length_g,length_g_2
         HElement_t :: hel,H0tmp
 
         !Divvy up the ij pairs
@@ -5845,7 +5845,8 @@ MODULE FciMCParMod
                     if(abs(kz).gt.NMAXZ) cycle
                     if(tGCutoff) then
                         length_g=real((kx-kj(1))**2+(ky-kj(2))**2+(kz-kj(3))**2)
-                        if(length_g.gt.gCutoff) cycle
+                        length_g_2=real((kx-ki(1))**2+(ky-ki(2))**2+(kz-ki(3))**2)
+                        if(length_g.gt.gCutoff.and.length_g_2.gt.gCutoff) cycle
                     endif
                     length=real((kx**2)+(ky**2)+(kz**2))
                     if(length.gt.OrbECutoff) cycle
@@ -5894,7 +5895,8 @@ MODULE FciMCParMod
                     if(abs(kz).gt.NMAXZ) cycle
                     if(tGCutoff) then
                         length_g=real((kx-kj(1))**2+(ky-kj(2))**2+(kz-kj(3))**2)
-                        if(length_g.gt.gCutoff) cycle
+                        length_g_2=real((kx-ki(1))**2+(ky-ki(2))**2+(kz-ki(3))**2)
+                        if(length_g.gt.gCutoff.and.length_g_2.gt.gCutoff) cycle
                     endif
                     length=real((kx**2)+(ky**2)+(kz**2))
                     if(length.gt.OrbECutoff) cycle
