d1f03a82644473b890aa6b6360410192fc77f463
ghb24@cam.ac.uk <ghb24@bari.ch.cam.ac.uk>
2011-04-05 15:22:53 +0100

    Fix two small bugs
    Alex's approach to fixing the CPMD/NECI interface issue
    with int64's fails for when NECI is compiled without the
    -i8 flag. This should be fixed now.
    
    Damn explicit interfaces... type checking is for losers.

--- src/lib/MemoryManagement.F90
+++ src/lib/MemoryManagement.F90
@@ -1,4 +1,5 @@
 module MemoryManager
+use constants , only : sizeof_int 
 
 ! JSS.  Memory book-keeping routines.  Contains a few elements of the initialisation, 
 ! output and structure of the memory_manager module from CamCASP (formerly SITUS), 
@@ -76,7 +77,7 @@ public :: TagIntType
 
 
 
-integer, parameter :: TagIntType = selected_int_kind(18)   !This is for CPMD which needs to know what type of integer to pass as a tag
+integer, parameter :: TagIntType = sizeof_int   !This is for CPMD which needs to know what type of integer to pass as a tag
 ! Configuration.
 integer, parameter :: MaxLen = 5000 ! size of memory log (max number of arrays
                                     ! that can be logged at any one time if 
