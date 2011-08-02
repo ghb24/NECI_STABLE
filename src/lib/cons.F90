9236b057e8ffe75a35f6a8268e4e73a3d5ef1a93
ghb24@cam.ac.uk <ghb24@bari.ch.cam.ac.uk>
2011-08-02 13:47:18 +0100

    Optimisation of part of the HPHF RDM stuff.
    Other potential optimisations? :
    
    1) Obtaining excitation matrix for different spin-coupled dets can
    be simplified, rather than doing it all again for the different
    combos?
    
    2) the four connections can be broken into two spin-equivalent
    connections. We could only sample one of the connections, and
    then spin-equivalence at the end?

--- src/lib/cons.F90
+++ src/lib/cons.F90
@@ -15,6 +15,8 @@ integer, parameter :: int64 = selected_int_kind(18)
 real(dp), parameter ::  PI    = 3.1415926535897932384626433832795028841971693993751_dp
 real(dp), parameter ::  PI2   = 9.8696044010893586188344909998761511353136994072408_dp
 real(dp), parameter ::  THIRD = 0.3333333333333333333333333333333333333333333333333_dp
+!real(dp), parameter ::  Root2 = 1.4142135623730950488016887242096980785696718753769_dp
+real(dp), parameter ::  Root2 = sqrt(2.0_dp) 
 
 integer :: temp=0
 integer, parameter :: sizeof_int = bit_size(temp)/8 !Bytes in a standard unspecified integer
