51de3cd0ac2c752effab9958794cc9a1f6b36772
George Booth <ghb24@cam.ac.uk>
2011-09-02 14:18:31 +0100

    Add include file for molpro integration.

---
+++ src/neci_molpro.h
@@ -0,0 +1,20 @@
+#ifdef MOLPRO
+
+#ifndef DSFMT_MEXP
+#define DSFMT_MEXP 19937
+#endif
+
+#define POINTER8
+#define HAVE_SSE2
+#define __INT64
+
+#ifndef HElement_t
+#define HElement_t real(dp)
+#endif
+
+#ifdef _MOLCAS_MPP_
+#define __SHARED_MEM
+#define PARALLEL
+#endif
+
+#endif /* MOLPRO */
