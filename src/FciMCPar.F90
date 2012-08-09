d92460bbc9bab5c2960249d6bce6c74e87c9878d
Simon Smart <sds50@cam.ac.uk>
2012-08-09 14:31:47 +0100

    Fix output real/integer mismatch

--- src/FciMCPar.F90
+++ src/FciMCPar.F90
@@ -1606,12 +1606,12 @@ MODULE FciMCParMod
         CALL MPIBcast(DetPos,NIfTot+1,int(HighPopOutPos(2)))
 
         if (iProcIndex == 0) then
-            write (iout, '(a, i10, a)') 'The most highly populated determinant &
+            write (iout, '(a, f10.4, a)') 'The most highly populated determinant &
                                   & with the opposite sign to the HF has ', &
                                   HighPopoutNeg(1), ' walkers.'
             call WriteBitDet (iout, DetNeg, .true.)
 
-            write (iout, '(a, i10, a9)') 'The most highly populated determinant &
+            write (iout, '(a, f10.4, a9)') 'The most highly populated determinant &
                                   & with the same sign as the HF has ', &
                                   HighPopoutPos(1), ' walkers.'
             call WriteBitDet (iout, DetPos, .true.)
