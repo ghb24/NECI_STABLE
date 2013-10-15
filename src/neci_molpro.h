2962e7bd1cd3be4b2515c892f68e2cc3ad6a46b3
George Booth <ghb24@cam.ac.uk>
2013-10-15 10:30:22 +0100

    Fix molpro bug

--- src/neci_molpro.h
+++ src/neci_molpro.h
@@ -12,15 +12,19 @@
 #define HElement_t real(dp)
 #endif
 
+#ifdef _COMMENT
 /* Parallel compilation in molpro with MPI */
 /* #if defined(_MOLCAS_MPP_) && !defined(GA_TCGMSG) && !defined(GA_TCGMSG5) - TCGMSG GA no longer supported */
+#endif
 #if defined(_MOLCAS_MPP_)
 
 #define PARALLEL
 #define CBINDMPI
 
 #if !defined(__OPEN64__) && !defined(__OLD_PGI__)
+#ifdef _COMMENT
 /* __OLD_PGI__ refers to bugs in pgi v. 7->8: see bugzilla 3616 */
+#endif
 #define __SHARED_MEM
 #endif
 
@@ -30,4 +34,4 @@
 #define __ISO_C_HACK
 #endif
 
-#endif /* MOLPRO */
+#endif
