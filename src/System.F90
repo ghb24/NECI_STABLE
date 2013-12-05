6686f685b2ef6b0ed156f45487315af37e086830
George Booth <ghb24@cam.ac.uk>
2013-12-05 05:35:23 +0000

    Automatic merge of stable neci branch to github server.

--- src/System.F90
+++ src/System.F90
@@ -370,6 +370,9 @@
 ! Copyright (c) 2013, Ali Alavi unless otherwise noted.
 ! This program is integrated in Molpro with the permission of George Booth and Ali Alavi
  
+! Copyright (c) 2013, Ali Alavi unless otherwise noted.
+! This program is integrated in Molpro with the permission of George Booth and Ali Alavi
+ 
 #include "macros.h"
 MODULE System
 
