30268422d4d0370637e268f96490ec34de772c90
Simon Smart <S.Smart@fkf.mpg.de>
2015-07-07 10:04:44 +0200

    Ensure that the molcas_prep script correctly makes cpp filenames lowercase

--- tools/molcas_prep.py
+++ tools/molcas_prep.py
@@ -120,7 +120,7 @@ def process_cpp(dir, fn, tgt_dir, tmp_dir):
         return;
 
     src_file = os.path.join(dir, fn)
-    tgt_file = os.path.join(tgt_dir, "{0}.c".format(root))
+    tgt_file = os.path.join(tgt_dir, "{0}.c".format(root.lower()))
     shutil.copyfile(src_file, tgt_file)
 
 def append_git_version(tgt_file):
