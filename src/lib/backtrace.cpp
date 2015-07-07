3e0a2b44b27337269797d6902fee2520091afcbd
Simon Smart <S.Smart@fkf.mpg.de>
2015-07-07 09:26:30 +0200

    C89 compatibility in backtrace.cpp

--- src/lib/backtrace.cpp
+++ src/lib/backtrace.cpp
@@ -24,7 +24,8 @@ void print_backtrace_neci ()
 	printf ("Writing Backtrace\n");
 	printf ("-----------------------------------\n");
 	// n.b. We don't include the function 'print_backtrace' in the backtrace.
-	for (int i = 1; i < n; i++)
+	int i;
+	for (i = 1; i < n; i++)
 		printf("%d: %s\n", i-1, strs[i]);
 	printf ("-----------------------------------\n");
 	free (strs);
