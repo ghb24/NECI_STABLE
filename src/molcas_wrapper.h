74dc56c82d525832bd1b2f5d0f6cd1337bf93899
Giovanni Li Manni <G.LiManni@fkf.mpg.de>
2015-07-07 07:35:15 +0200

    NECI GIT version passed to molcas flags at templating step (molcas_prep.py) via molcas_wrapper.h file

--- src/molcas_wrapper.h
+++ src/molcas_wrapper.h
@@ -1,6 +1,5 @@
 #ifdef _MOLCAS_
 
-
 #define HElement_t real(dp)
 #define __DOUBLERUN
 #define DISABLE_FFTW
@@ -10,16 +9,14 @@
 #define __Linux
 #define DSFMT_MEXP 19937
 #define DDISABLE_FFTW
-
+#define _CONFIG Molcas
 
 #ifdef _MOLCAS_MPP
 #define PARALLEL
 #endif
 
-
 #ifdef _INTEL_
 #define __IFORT
 #endif
-
 
 #endif
