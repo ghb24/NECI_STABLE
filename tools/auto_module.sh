0f4e93242da0a99f1fb86a238e3738b4a9b385be
Simon Smart <S.Smart@fkf.mpg.de>
2015-07-16 17:42:18 +0200

    Add gfortran to the testing auto-module-configuration script

--- tools/auto_module.sh
+++ tools/auto_module.sh
@@ -9,6 +9,8 @@ echo "Loading modules for: $@"
 
 if [ "fkf-ifort -g" == "$@" ] || [ "fkf-ifort" == "$@" ]; then
 	module load ifort/15.0.3 mpi.intel/5.0.3
+elif [ "gfortran-simple -g" == "$@" ] || [ "gfortran-simple" == "$@" ]; then
+	module load gnu-openmpi/1.7.2
 else
 	echo "Module configuration not set"
 fi
