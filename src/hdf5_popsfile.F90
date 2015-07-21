91c7505a9e794c5bc94449e37024a0452545fd20
Simon Smart <simondsmart@gmail.com>
2015-07-21 14:22:30 +0100

    Added random number details

--- src/hdf5_popsfile.F90
+++ src/hdf5_popsfile.F90
@@ -9,23 +9,48 @@ module hdf5_popsfile
     !   --> Don't assume that only walker-data is going to be relevant
     !   --> Create groups appropriately.
     !
-    ! /system/                - Details of the system that is being restarted
+    ! A: vcs_ver             - The SHA ID of the git commit
+    ! A: compiled_at         - The time of code compilation
+    ! A: compiled_on         - The date of code compilation
+    ! A: config              - The build configuration
+    ! A: status              - (opt) indicates if local changes beyond SHA ID
     !
-    ! /calculation/           - Details of the calculation
+    ! /system/               - Details of the system that is being restarted
+    !
+    ! /calculation/          - Details of the calculation
+    !     /random_hash/      - Random values used in the orbital mapping
     ! 
-    ! /determinants/A         - Details of a determinental Hilbert space
-    !     A: 
+    ! /wavefunction/         - Details of a determinental Hilbert space
+    !     A: width           - Width of the bit-rep in 64-bit integers
+    !
+    !     /ilut/             - The bit representations of the determinants
+    !     /sgns/             - The occupation of the determinants
 
+    use Parallel_neci
     use constants
     use hdf5_util
+    use util_mod
     use hdf5
     implicit none
     private
 
     ! Constants for naming various sections
     character(*), parameter :: &
+
+            nm_vcs_ver = 'vcs_ver', &
+            nm_comp_time = 'compiled_at', &
+            nm_comp_date = 'compiled_on', &
+            nm_comp_config = 'config', &
+            nm_comp_status = 'status', &
+
+            nm_calc_grp = 'calculation', &
+            nm_random_hash = 'random_hash', &
             
-            wfn_grp = 'wavefunction'
+            nm_wfn_grp = 'wavefunction', &
+            nm_rep_width = 'width', &
+            nm_sgn_len = 'lenof_sign', &
+            nm_ilut = 'ilut', &
+            nm_sgns = 'sgns'
 
     public :: write_popsfile_hdf5, read_popsfile_hdf5
 
@@ -33,14 +58,23 @@ contains
 
     subroutine write_popsfile_hdf5()
 
-        integer(hid_t) :: file_id, err
+        integer(hid_t) :: plist_id, file_id, err
 
         ! Initialise the hdf5 fortran interface
         call h5open_f(err)
 
-        ! TODO: Do sensible file handling here...
-        call h5fcreate_f('popsfile.hdf5', H5F_ACC_TRUNC_F, file_id, err)
+        ! Set up a property list to ensure file handling across all nodes.
+        ! TODO: Check if we should be using a more specific communicator
+        call h5pcreate_f(H5P_FILE_ACCESS_F, plist_id, err)
+        call h5pset_fapl_mpio_f(plist_id, MPI_COMM_WORLD, MPI_INFO_NULl, err)
 
+        ! TODO: Do sensible file handling here...
+        call h5fcreate_f('popsfile.hdf5', H5F_ACC_TRUNC_F, file_id, err, &
+                         access_prp=plist_id)
+        call h5pclose_f(plist_id, err)
+
+        call write_metadata(file_id)
+        call write_calc_data(file_id)
         call write_walkers(file_id)
 
         ! And we are done!
@@ -55,19 +89,123 @@ contains
     end subroutine
 
 
+    subroutine write_metadata(parent)
+
+        ! Output macroscopic metadata applicable to a restart file, which may
+        ! be used for establishing providence of calculations, etc.
+
+        integer(hid_t), intent(in) :: parent
+
+        ! Run by
+        ! Date
+        ! SHAid
+        ! Sequence number
+        call write_string_attribute(parent, "hi", "This is a test")
+        call write_string_attribute(parent, nm_vcs_ver, _VCS_VER)
+        call write_string_attribute(parent, nm_comp_date, __DATE__)
+        call write_string_attribute(parent, nm_comp_time, __TIME__)
+        call write_string_attribute(parent, nm_comp_config, _CONFIG)
+#ifdef _WORKING_DIR_CHANGES
+        call write_string_attribute(parent, nm_comp_status, &
+                                    "Working directory contains local changes")
+#endif
+
+    end subroutine
+
+    subroutine write_calc_data(parent)
+
+        use load_balance_calcnodes, only: RandomOrbIndex
+        use iso_c_hack
+        integer(hid_t), intent(in) :: parent
+        integer(hid_t) :: calc_grp, err
+
+        ! Firstly create the group for storing calculation-related data
+        call h5gcreate_f(parent, nm_calc_grp, calc_grp, err)
+
+        ! Write out the random orbital mapping index
+        call write_int64_1d_dataset(calc_grp, nm_random_hash, RandomOrbIndex)
+
+        ! Clear stuff up
+        call h5gclose_f(calc_grp, err)
+
+    end subroutine
+
     subroutine write_walkers(parent)
+
+        use iso_c_hack
+        use bit_rep_data, only: NIfD, NIfTot, NOffSgn
+        use FciMCData, only: AllTotWalkers, CurrentDets, MaxWalkersPart, &
+                             TotWalkers
+        use CalcData, only: tUseRealCoeffs
 
         ! Output the wavefunction information to the relevant groups in the
         ! wavefunction.
 
         integer(hid_t), intent(in) :: parent
+        type(c_ptr) :: cptr
+        integer(int32), pointer :: ptr(:)
+        integer(int32) :: boop
 
-        integer(hid_t) :: wfn_grp_id, err
+        character(*), parameter :: t_r = 'write_walkers'
 
+        integer(hid_t) :: wfn_grp_id, dataspace, dataset, err, memspace
+        integer(hid_t) :: plist_id
+
+        integer(hsize_t) :: counts(0:nProcessors-1)
+        integer(hsize_t) :: all_count
+
+        integer(int32) :: bit_rep_width
+        integer(hsize_t) :: mem_offset(2), write_offset(2)
+        integer(hsize_t) :: dims(2), hyperdims(2)
+        integer :: ierr
+
+        ! TODO: Add a (slower) fallback routine for weird cases, odd HDF libs
 
         ! Firstly create the group for storing wavefunction info
-        call h5gcreate_f(parent, wfn_grp, wfn_grp_id, err)
+        call h5gcreate_f(parent, nm_wfn_grp, wfn_grp_id, err)
 
+        ! TODO: Refactor these chunks into their own little subroutines.
+        ! We fix the format of the binary file. Thus if we are on a 32-bit
+        ! build, we need to convert the data into 64-bit compatibile chunks.
+        if (build_64bit) then
+            bit_rep_width = NIfD + 1
+        else
+            bit_rep_width = 2 * (NIfD + 1)
+            call stop_all(t_r, "Needs manual, careful, testing")
+        end if
+
+        ! Output the bit-representation data
+        call write_int32_attribute(wfn_grp_id, nm_rep_width, bit_rep_width)
+        call write_int32_attribute(wfn_grp_id, nm_sgn_len, &
+                                   int(lenof_sign, int32))
+
+        ! How many occuiped determinants are there on each of the processors
+        call MPIAllGather(TotWalkers, counts, ierr)
+        all_count = sum(counts)
+        write_offset = [0_hsize_t, sum(counts(0:iProcIndex-1))]
+
+        ! Write out the determinant bit-representations
+        call write_2d_multi_arr_chunk_offset( &
+                wfn_grp_id, nm_ilut, H5T_NATIVE_INTEGER_8, &
+                arr_2d_ptr(CurrentDets), arr_2d_dims(CurrentDets), &
+                [int(nifd+1, hsize_t), int(TotWalkers, hsize_t)], & ! dims
+                [0_hsize_t, 0_hsize_t], & ! offset
+                [int(nifd+1, hsize_t), all_count], & ! all dims
+                [0_hsize_t, sum(counts(0:iProcIndex-1))] & ! output offset
+        )
+
+        ! Write out the sign values on each of the processors
+        if (.not. tUseRealCoeffs) &
+            call stop_all(t_r, "This could go badly...")
+
+        call write_2d_multi_arr_chunk_offset( &
+                wfn_grp_id, nm_sgns, H5T_NATIVE_REAL_8, &
+                arr_2d_ptr(CurrentDets), arr_2d_dims(CurrentDets), &
+                [int(lenof_sign, hsize_t), int(TotWalkers, hsize_t)], & ! dims
+                [int(nOffSgn, hsize_t), 0_hsize_t], & ! offset
+                [int(lenof_sign, hsize_t), all_count], & ! all dims
+                [0_hsize_t, sum(counts(0:iProcIndex-1))] & ! output offset
+        )
 
         ! And we are done
         call h5gclose_f(wfn_grp_id, err)
--- src/lib/hdf5_util.F90
+++ src/lib/hdf5_util.F90
@@ -12,7 +12,9 @@ module hdf5_util
     ! guarantee compatibility across programming languages, compilers and
     ! build configurations, then it helps to be explicit.
 
+    use iso_c_hack
     use constants
+    use util_mod
     use hdf5
     implicit none
 
@@ -20,7 +22,7 @@ contains
 
     subroutine write_int32_attribute(parent, nm, val)
 
-        integer(hid_t), intent(inout) :: parent
+        integer(hid_t), intent(in) :: parent
         character(*), intent(in) :: nm
         integer(int32), intent(in) :: val
 
@@ -41,5 +43,121 @@ contains
         call h5sclose_f(dataspace, err)
 
     end subroutine
+
+    subroutine write_string_attribute(parent, nm, val)
+
+        integer(hid_t), intent(in) :: parent
+        character(*), intent(in) :: nm
+        character(*), intent(in) :: val
+
+        integer(hid_t) :: dataspace, attribute, type_id, err
+
+        ! Create an HDF type associated with a fortran string of _exactly_
+        ! this length
+        call h5tcopy_f(H5T_FORTRAN_S1, type_id, err)
+        call h5tset_size_f(type_id, len(val, SIZE_T), err)
+
+        ! Create a dataspace, and attribute, and write the string
+        call h5screate_f(H5S_SCALAR_F, dataspace, err)
+        call h5acreate_f(parent, nm, type_id, dataspace, attribute, err)
+        call h5awrite_f(attribute, type_id, val, [1_hsize_t], err)
+        call h5aclose_f(attribute, err)
+        call h5sclose_f(dataspace, err)
+        call h5tclose_f(type_id, err)
+
+    end subroutine
+
+    subroutine write_int64_1d_dataset(parent, nm, val)
+
+        ! Write out a 64-bit array
+        ! TODO: It may be worth refactoring multiple-types to remove the
+        !       generic array writing code from the specific.
+
+        integer(hid_t) :: parent
+        character(*), intent(in) :: nm
+        integer(int64), intent(in), target :: val(:)
+
+        integer(hid_t) :: dataspace, dataset, err
+        integer(hsize_t) :: dims(1)
+        integer(int32), pointer :: ptr(:)
+
+        ! Create the appropriate dataspace
+        dims = [ubound(val) - lbound(val) + 1]
+        call h5screate_simple_f(1_hid_t, dims, dataspace, err)
+
+        ! Create the dataset with the correct type
+        call h5dcreate_f(parent, nm, H5T_NATIVE_INTEGER_8, dataspace, &
+                         dataset, err)
+
+        ! write the data
+        call int32_pointer_abuse(val, ptr)
+        call h5dwrite_f(dataset, H5T_NATIVE_INTEGER_8, ptr, dims, err)
+
+        ! Close the residual handles
+        call h5dclose_f(dataset, err)
+        call h5sclose_f(dataspace, err)
+
+    end subroutine
+
+    subroutine write_2d_multi_arr_chunk_offset( &
+                       parent, nm, itype, cptr, arr_dims, mem_dims, mem_offset, &
+                       dataspace_dims, dataspace_offset)
+
+        ! Write a chunk of memory from each of the MPI processes into the
+        ! specified place in the output file.
+        !
+        ! mem_dims   - the dimensions of the chunk of the memory array that we
+        !              want to write
+        ! mem_Offset - the offset of the aforementioned chunk from the start
+        !              of the array
+        ! dataspace_dims
+        !            - the dimensions of the overall dataspace
+        ! dataspace_offset
+        !            - the offset at which we want to write this data.
+
+        integer(hid_t), intent(in) :: parent, itype
+        character(*), intent(in) :: nm
+        type(c_ptr), intent(in) :: cptr
+        integer(hsize_t), intent(in) :: arr_dims(2)
+        integer(hsize_t), intent(in) :: mem_dims(2), mem_offset(2)
+        integer(hsize_t), intent(in) :: dataspace_dims(2), dataspace_offset(2)
+
+        integer(hsize_t) :: dims(2)
+        integer(hid_t) :: memspace, dataspace, dataset, plist_id, err
+        integer(int32), pointer :: ptr(:)
+
+        ! Create the source (memory) dataspace, and select the appropriate
+        ! hyperslab inside it
+        call h5screate_simple_f(2_hid_t, arr_dims, memspace, err)
+        call h5sselect_hyperslab_f(memspace, H5S_SELECT_SET_F, mem_offset, &
+                                   mem_dims, err)
+
+        ! Create an array in the target file with the size of the total amount
+        ! to be written out, across the processors
+        call h5screate_simple_f(2_hid_t, dataspace_dims, dataspace, err)
+        call h5sselect_hyperslab_f(dataspace, H5S_SELECT_SET_F, &
+                                   dataspace_offset, mem_dims, err)
+
+        ! Create a property list to do multi-process collective writes
+        call h5pcreate_f(H5P_DATASET_XFER_F, plist_id, err)
+        call h5pset_dxpl_mpio_f(plist_id, H5FD_MPIO_COLLECTIVE_F, err)
+
+        ! Create the dataset with the correct type
+        call h5dcreate_f(parent, nm, itype, dataspace, dataset, err)
+
+        ! Get access to a pointer to the array that will always be considered
+        ! to have a valid type by the HDF5 library. For some reason the
+        ! 64-bit, 2d array is not always given an interface...
+        call c_f_pointer(cptr, ptr, [1])
+
+        call h5dwrite_f(dataset, itype, ptr, arr_dims, err, memspace, &
+                        dataspace, xfer_prp=plist_id)
+
+        call h5dclose_f(dataset, err)
+        call h5pclose_f(plist_id, err)
+        call h5sclose_f(dataspace, err)
+        call h5sclose_f(memspace, err)
+
+    end subroutine write_2d_multi_arr_chunk_offset
 
 end module
