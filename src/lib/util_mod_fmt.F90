ghb24/NECI_STABLE

commit 6332f3250d02e9ad3c8c2b4f9684f5b78fee259c

Add omitted file

src/lib/util_mod_fmt.F90
@@ -1,0 +1,84 @@
+module fmt_utils
+
+    use constants
+    implicit none
+    private
+
+    interface int_fmt
+        module procedure int64_fmt
+        module procedure int32_fmt
+    end interface
+
+    public :: int_fmt
+
+contains
+
+    function int32_fmt(i, padding) result(fmt_str)
+
+        ! Return the format string that exactly contains the specified
+        ! integer
+
+        integer(int32), intent(in) :: i
+        integer, intent(in), optional :: padding
+        character(4) :: fmt_str
+        real(dp) :: logi
+
+        if (i == 0 .or. i == 1) then
+            logi = 1.0
+        else
+            logi = log10(real(abs(i), dp))
+        end if
+        if (i < 0) logi = logi + 1
+
+        fmt_str = int_fmt_local(logi)
+
+    end function
+
+    function int64_fmt(i, padding) result(fmt_str)
+
+        ! Return the format string that exactly contains the specified
+        ! integer
+
+        integer(int64), intent(in) :: i
+        integer, intent(in), optional :: padding
+        character(4) :: fmt_str
+        real(dp) :: logi
+
+        if (i == 0 .or. i == 1) then
+            logi = 1.0
+        else
+            logi = log10(real(abs(i), dp))
+        end if
+        if (i < 0) logi = logi + 1
+
+        fmt_str = int_fmt_local(logi)
+
+    end function
+
+    function int_fmt_local(logi, padding) result(fmt_str)
+
+        ! Return the format string that exactly contains the specified
+        ! integer (local worker function)
+
+        real(dp), intent(in) :: logi
+        integer, intent(in), optional :: padding
+        character(4) :: fmt_str
+        integer :: ndigit
+
+        ndigit = 1 + int(floor(logi))
+
+        if (present(padding)) ndigit = ndigit + padding
+
+        if (ndigit < 10) then
+            write(fmt_str, '("i",i1)') ndigit
+        else if (ndigit < 100) then
+            write(fmt_str, '("i",i2)') ndigit
+        else
+            ! n.b. should never hit this. Will hit integer overflow first
+            write(fmt_str, '("i",i3)') ndigit
+        end if
+
+    end function
+
+
+end module

commit 802fde45a560506654258ecf9a0b381d17d6a5d2

Fix bug introduced into int_fmt in commit 6332f3250d02
In commit 6332f3250d02 a change was made to int_fmt which gave the
incorrect format ('i2' instead of 'i1') for an input of 1, and also
stopped padding from being used when asked.

src/lib/util_mod_fmt.F90
@@ -26,11 +26,11 @@ contains
         if (i == 0 .or. i == 1) then
             logi = 1.0
         else
-            logi = log10(real(abs(i), dp))
+            logi = log10(real(abs(i)+1.0, dp))
         end if
         if (i < 0) logi = logi + 1
 
-        fmt_str = int_fmt_local(logi)
+        fmt_str = int_fmt_local(logi, padding)
 
     end function
 
@@ -47,11 +47,11 @@ contains
         if (i == 0 .or. i == 1) then
             logi = 1.0
         else
-            logi = log10(real(abs(i), dp))
+            logi = log10(real(abs(i)+1.0, dp))
         end if
         if (i < 0) logi = logi + 1
 
-        fmt_str = int_fmt_local(logi)
+        fmt_str = int_fmt_local(logi, padding)
 
     end function
 
@@ -65,7 +65,7 @@ contains
         character(4) :: fmt_str
         integer :: ndigit
 
-        ndigit = 1 + int(floor(logi))
+        ndigit = int(ceiling(logi))
 
         if (present(padding)) ndigit = ndigit + padding
 