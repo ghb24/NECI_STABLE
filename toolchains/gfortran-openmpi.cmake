83c635561e508565439d751905f8912e41a57115
Simon Smart <simondsmart@gmail.com>
2016-03-28 19:04:52 +0100

    Add some commentary

--- toolchains/gfortran-openmpi.cmake
+++ toolchains/gfortran-openmpi.cmake
@@ -12,13 +12,6 @@ CMAKE_FORCE_C_COMPILER       ( gcc GNU )
 CMAKE_FORCE_CXX_COMPILER     ( g++ GNU )
 CMAKE_FORCE_Fortran_COMPILER ( mpif90 GNU )
 
-# Set the default implicit linker flags for the different languages 
-
-#set( CMAKE_IMPLICIT_CXX_LINK_LIBRARIES stdc++ m c )
-#set( CMAKE_IMPLICIT_Fortran_LINK_LIBRARIES gfortran m quadmath m c )
-
-# TODO: Compiler flags!!!
-
 # Override MPI searching. Override arbitrary package searching...
 
 set( NECI_FIND_MPI OFF )
@@ -26,13 +19,30 @@ set( NECI_FIND_LibRT OFF )
 
 # Compile flags
 
-set( NECI_CXX_FLAGS_RELEASE -O9 -help )
-
-# Cluster compilation flags
-
-# Debug flags
+# Arbitrary compile flags can be overriden here.
+# e.g.
+#
+# set( NECI_CXX_FLAGS_RELEASE -O3 )   # Release mode specific CXX flags (also: DEBUG, CLUSTER)
+# set( NECI_Fortran_FLAGS -O3 )       # All build-type fortran flags
 
 # Link flags
 
+# Arbitrary compile flags can be overriden here.
+# e.g.
+#
+# set( NECI_CXX_LINK_FLAGS -O3 )
+
+# Linker libraries
+
+# Arbitrary flags are set here. Note that these are set by LANGUAGE and TARGET TYPE. They are set inside
+# add_library or add_executable, and so only take impact if the LINKER_LANGUAGE setting is set on those
+# types.
+#
+# e.g.
+# set ( NECI_Fortran_STATIC_LINK_LIBRARIES fftw3 )     # Impacts static libraries
+# set ( NECI_Fortran_EXE_LINK_LIBRARIES stdc++ )       # Impacts executables
+# set ( NECI_Fortran_LINK_LIBRARIES m )                # Impacts both libraries and executables
+
 # By using CMakeForceCompiler, we break the autodetction of the c++ library requirement
+# (STATIC --> adds these only to libneci, libkneci, ... not directly to the executables)
 set( NECI_Fortran_STATIC_LINK_LIBRARIES stdc++ rt )
