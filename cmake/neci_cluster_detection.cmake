2ebe0c12f4177afe43c4cae29a85542908001c2c
Simon Smart <simondsmart@gmail.com>
2016-03-29 21:07:45 +0100

    Add the cluster detection cmake file

---
+++ cmake/neci_cluster_detection.cmake
@@ -0,0 +1,47 @@
+#
+# Automatic detection of various clusters
+# --> Make automagical modifications to the build configuration, set global variables, and/or enforce that
+#     we are using certain toolchain files
+
+# Cluster detection ultimately depends on hostname checking ... imprecise, but hey.
+site_name(_hostname)
+
+# Special settings for archer
+
+if( ${_hostname} MATCHES "eslogin[0-9]" )
+
+	message("***************************************************************************************")
+	message("Compilation on Archer detected")
+	message("-- Archer is a somewhat weird system, with wrapper scripts that defeat autodetection")
+	message("--")
+	message("-- Ensure that the correct modules are loaded, then:")
+	message("-- Use the module cray-hdf5-parallel for HDF5 support")
+	message("-- Run cmake with -DCMAKE_TOOLCHAIN_FILE=<neci_dir>/toolchains/archer.cmake")
+	message("-- Normal MPI detection is overridden. If the compiler is not specified, this will fail")
+	message("***************************************************************************************")
+
+    set( ${PROJECT_NAME}_KNOWN_CLUSTER "Archer" )
+
+    if ( DEFINED CMAKE_TOOLCHAIN_FILE )
+        get_filename_compontent( _toolchain_file ${CMAKE_TOOLCHAIN_FILE} NAME )
+    endif()
+
+    if ( NOT DEFINED CMAKE_TOOLCHAIN_FILE 
+         NOT _toolchain_file STREQUAL "archer.cmake" )
+        message(FATAL_ERROR "Compiling on Archer requires the archer.cmake toolchain file")
+    endif()
+
+endif()
+
+
+if( ${_hostname} MATCHES "hydra[0-9]" )
+    
+    set( ${PROJECT_NAME}_KNOWN_CLUSTER "Hydra" )
+
+    if( CMAKE_Fortran_COMPILER_ID STREQUAL "Intel" )
+        message( STATUS "Changing RELEASE optimisation flags as Hydra is an inhomogeneous cluster" )
+        list( REMOVE CMAKE_Fortran_FLAGS_RELEASE "-xHost" )
+        list( APPEND CMAKE_Fortran_FLAGS_RELEASE "-xavx" )
+    endif()
+
+endif()
