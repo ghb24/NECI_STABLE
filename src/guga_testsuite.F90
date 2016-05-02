8ee8ea0ebb30c11fcf332c5986327220ce752f91
Werner Dobrautz <w.dobrautz@fkf.mpg.de>
2016-05-02 17:55:25 +0200

    changed a bug in error analyis
    if lenof_sing is 2, the error analysis code assumes the complex
    code is in use, but this could also be only a mneci run..
    some cleaning up has to be done there i guess

--- src/guga_testsuite.F90
+++ src/guga_testsuite.F90
@@ -201,8 +201,8 @@ contains
             ! matrix element consistency! 
 !             call run_test_excit_gen_guga_multiple(&
 !                 [1,2,3,4,5,6,7,8,15,16])
-!             call run_test_excit_gen_guga_general
-            call run_test_excit_gen_guga_single([1,3,4,5,7,10,11,12])
+            call run_test_excit_gen_guga_general
+!             call run_test_excit_gen_guga_single([1,3,4,5,7,10,11,12])
 !             call run_test_excit_gen_guga_multiple(&
 !                 [1,4,5,8,9,12,13,16,17,20])
 !             call run_test_excit_gen_guga_single(&
