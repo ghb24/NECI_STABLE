c5ae2c68ed5097969be8c0db28add79d0d8cfc97
Nick Blunt <nsb37@cam.ac.uk>
2016-05-03 15:00:33 +0100

    Fix silly bug - make sure close statements are within if-statement.

--- src/rdm_finalising.F90
+++ src/rdm_finalising.F90
@@ -982,7 +982,9 @@ contains
                     end do
 
                     close(iunit_aaaa); close(iunit_abab); close(iunit_abba); close(iunit_abab);
-                    if (open_shell) close(iunit_bbbb); close(iunit_baba); close(iunit_baab); close(iunit_abba);
+                    if (open_shell) then
+                        close(iunit_bbbb); close(iunit_baba); close(iunit_baab); close(iunit_abba);
+                    end if
                 end if
             end do
 
