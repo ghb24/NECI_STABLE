51ebad8df08a3d7db4528a9d046e372535e86791
Werner Dobrautz <w.dobrautz@fkf.mpg.de>
2017-07-10 16:05:57 +0200

    changed to only check compiling hdf5
    standalone on the altest-server

--- tools/auto_make.sh
+++ tools/auto_make.sh
@@ -7,7 +7,7 @@
 echo "Calling make for $@"
 
 if [ "fkf-ifort hdf5" == "$@" ] || [ "gfortran-hdf5" == "$@" ]; then 
-	make VERBOSE=1 hdf5 neci 
+	make VERBOSE=1 hdf5
 else
 	make VERBOSE=1 -j2 neci mneci kneci kmneci dneci test_neci test_mneci test_kneci test_kmneci test_dneci
 fi
