f867b3edf34144a58373dac47a5019b1b5c16fe6
Werner Dobrautz <w.dobrautz@fkf.mpg.de>
2017-07-11 11:57:03 +0200

    one more try with ifort + hdf5 download
    compilation

--- tools/auto_make.sh
+++ tools/auto_make.sh
@@ -6,7 +6,10 @@
 
 echo "Calling make for $@"
 
-if [ "fkf-ifort-hdf5" == "$@" ] || [ "gfortran-hdf5" == "$@" ]; then 
+if [ "fkf-ifort-hdf5" == "$@" ]; then
+	make VERBOSE=1 hdf5 
+	make VERBOSE=1 neci
+elif [ "gfortran-hdf5" == "$@" ]; then 
 	make VERBOSE=1 hdf5
 else
 	make VERBOSE=1 -j2 neci mneci kneci kmneci dneci test_neci test_mneci test_kneci test_kmneci test_dneci
