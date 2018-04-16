dafc30c1aaf815540b8c99cf7bcdbdd8ca4b8c24
Werner Dobrautz <w.dobrautz@fkf.mpg.de>
2018-04-16 15:16:16 +0200

    hdf5 compilation fix

--- cmake/FindHDF5_NECI.cmake
+++ cmake/FindHDF5_NECI.cmake
@@ -63,7 +63,7 @@ if ( HAVE_BUILD_HDF5 )
 		hdf5
 		# -- Download step ---
 		PREFIX ${HDF_DIR}-prefix
-		URL https://support.hdfgroup.org/ftp/HDF5/current18/src/hdf5.-1.8.20.tar.gz
+		URL https://support.hdfgroup.org/ftp/HDF5/current18/src/hdf5-1.8.20.tar.gz
 		URL_MD5 7f2d3fd67106968eb45d133f5a22150f
 
 	# previous 1.8.19 hash:
