aa42e1555a52fb06c28384b420aef0f3fe86be1e
Kai Guther <k.guther@fkf.mpg.de>
2018-09-24 15:13:54 +0200

    forgotten optimization

--- src/FciMCPar.F90
+++ src/FciMCPar.F90
@@ -1077,7 +1077,7 @@ module FciMCParMod
             end if
 
             if (tTruncInitiator) then
-                call CalcParentFlag (j, parent_flags)
+                call CalcParentFlag (j, DetCurr, WalkExcitLevel, parent_flags)
             end if
 
             !Debug output.
