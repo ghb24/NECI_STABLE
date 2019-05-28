c73f67a4abedfc5e65b05c63af104873cffbef24
Werner Dobrautz <w.dobrautz@fkf.mpg.de>
2019-05-28 13:14:51 +0200

    start working on GUGA RDMs

--- src/guga_rdm.F90
+++ src/guga_rdm.F90
@@ -7,6 +7,16 @@ module guga_rdm
 
 contains 
 
+
+    subroutine calc_explicit_1_rdm()
+        ! routine to calculate the one-RDM explicitly in the GUGA formalism
+        ! the one RDM is given by rho_ij = <Psi|E_ij|Psi> 
+        ! so, similar to the actHamiltonian routine I need to loop over all 
+        ! the orbital indices (i,j) and calculate the action of a given E_ij 
+        ! on the sampled wavefunction |Psi> and the resulting overlap with <Psi|
+
+    end subroutine calc_explicit_1_rdm
+
 end module guga_rdm
 
 #endif
