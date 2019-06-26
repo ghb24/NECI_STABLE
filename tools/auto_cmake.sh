47b22fa36496bf2afa4b738d4de19a5a54dfb64d
Kai Guther <k.guther@fkf.mpg.de>
2019-06-26 11:58:47 +0000

    auto_cmake.sh edited online with Bitbucket

--- tools/auto_cmake.sh
+++ tools/auto_cmake.sh
@@ -7,15 +7,21 @@
 
 echo "Calling cmake for: $@"
 
-if [ "gfortran-simple" == "$@" ] || [ "fkf-ifort" == "$@" ] || [ "fkf-ifort-new" == "$@" ]; then
+if [ "gfortran-simple" == "$@" ]; then
 	cmake -DENABLE_HDF5=ON -DCMAKE_BUILD_TYPE=Release ..
-elif [ "gfortran-simple -g" == "$@" ] || [ "fkf-ifort -g" == "$@" ]; then
+elif [ "fkf-ifort" == "$@" ] || [ "fkf-ifort-new" == "$@" ]; then
+	cmake -DENABLE_HDF5=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_COMPILER=mpiifort ..
+elif [ "gfortran-simple -g" == "$@" ]; then
 	cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_HDF5=ON ..
+elif [ "fkf-ifort -g" == "$@" ]; then
+	cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_HDF5=ON -DCMAKE_Fortran_COMPILER=mpiifort ..
 elif [ "pgi-simple" == "$@" ]; then
 	cmake -DENABLE_HDF5=ON -DCMAKE_BUILD_TYPE=Release -DENABLE_SHARED_MEMORY=off ..
 elif [ "pgi-simple -g" == "$@" ]; then
 	cmake -DENABLE_HDF5=ON -DCMAKE_BUILD_TYPE=Debug -DENABLE_SHARED_MEMORY=off ..
-elif [ "fkf-ifort-hdf5" == "$@" ] || [ "gfortran-hdf5" == "$@" ]; then 
+elif [ "fkf-ifort-hdf5" == "$@" ]; then
+	cmake -DENABLE_BUILD_HDF5=ON -DENABLE_HDF5=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_COMPILER=mpiifort ..
+elif [ "gfortran-hdf5" == "$@" ]; then 
 	cmake -DENABLE_BUILD_HDF5=ON -DENABLE_HDF5=ON -DCMAKE_BUILD_TYPE=Release ..
 else
 	echo "Module not executed"
