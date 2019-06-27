604b01c0d0092dc97e1a6589271234d23cad2b0e
Kai Guther <k.guther@fkf.mpg.de>
2019-06-27 19:51:26 +0200

    dont try to load non-existent modules (preliminary, modules to be added)

--- tools/auto_module.sh
+++ tools/auto_module.sh
@@ -8,17 +8,19 @@
 echo "Loading modules for: $@"
 
 if [ "fkf-ifort -g" == "$@" ] || [ "fkf-ifort" == "$@" ] || [ "fkf-ifort-new" == "$@" ]; then
-	module load ifort/18.0.1 mpi.intel/5.0.3 hdf5-intel
+        export HDF5_ROOT=/usr/lib/custom_hdf5_ifort
+        module load ifort/18.0.1 mpi.intel/5.0.3 #hdf5-intel
 elif [ "gfortran-simple -g" == "$@" ] || [ "gfortran-simple" == "$@" ]; then
         export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/mpi/gcc/openmpi3/lib64
-	module load gnu-openmpi/3.0.0 hdf5-gfortran/1.8.20
+	export HDF5_ROOT=/usr/lib/custom_hdf5_gfortran
+#	module load gnu-openmpi/3.0.0 hdf5-gfortran/1.8.20
 elif [ "pgi-simple -g" == "$@" ] || [ "pgi-simple" == "$@" ]; then
 	module load PrgEnv-pgi/15.4 hdf5-pgi
 elif [ "fkf-ifort-hdf5" == "$@" ]; then 
 	module load ifort/18.0.1 mpi.intel/5.0.3
 elif [ "gfortran-hdf5" == "$@" ]; then
         export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/mpi/gcc/openmpi3/lib64
-	module load gnu-openmpi/3.0.0
+#	module load gnu-openmpi/3.0.0
 elif [ "fkf-ifort-latest" == "$@" ]; then 
 	module load fkf-ifort mpi.intel
 else
