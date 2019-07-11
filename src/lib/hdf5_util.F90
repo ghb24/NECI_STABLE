8c932c7263dd49c3881466db3a122fa423fb127a
Oskar Weser <o.weser@fkf.mpg.de>
2019-07-11 11:44:27 +0200

    MAINT: change integer kind that denotes HDF5 datatypes

--- src/lib/hdf5_util.F90
+++ src/lib/hdf5_util.F90
@@ -1064,7 +1064,7 @@ contains
         ! before reading in.
 
         integer(hid_t), intent(in) :: dataset
-        integer :: class_id
+        integer(hdf_err) :: class_id
         character(*), intent(in) :: nm
         integer(hsize_t), intent(in) :: sz
         integer(hsize_t), intent(in) :: dims(:)
