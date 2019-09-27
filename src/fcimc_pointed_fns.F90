ae8a68c6965ce167a80d8af29abd21945b27ea83
Kai Guther <k.guther@fkf.mpg.de>
2019-09-27 10:36:03 +0200

    debug -> warning_workaround

--- src/fcimc_pointed_fns.F90
+++ src/fcimc_pointed_fns.F90
@@ -788,7 +788,7 @@ module fcimc_pointed_fns
       
       real(dp), intent(in) :: ci
       real(dp) :: Si
-#ifdef __DEBUG
+#ifdef _WARNING_WORKAROUND
       ! Disable compiler warnings
       real(dp) :: dummy
       dummy = ci
