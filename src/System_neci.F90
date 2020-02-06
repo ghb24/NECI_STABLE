8293c100c1a941155d5fd0d0f2ae1c0e172d1d97
Kai Guther <k.guther@fkf.mpg.de>
2020-02-06 17:11:02 +0100

    added guard flag to ensure the random orbital mapping has been initialized at popsfile read

--- src/System_neci.F90
+++ src/System_neci.F90
@@ -5,7 +5,7 @@ MODULE System
     use CalcData, only: TAU, tTruncInitiator, InitiatorWalkNo, &
                         occCASorbs, virtCASorbs, tPairedReplicas, tInitializeCSF, &
                         S2Init, tDynamicAvMcEx
-    use FciMCData, only: tGenMatHEl
+    use FciMCData, only: tGenMatHEl, t_initialized_roi
     use sort_mod
     use SymExcitDataMod, only: tBuildOccVirtList, tBuildSpinSepLists
     use constants
@@ -217,6 +217,7 @@ MODULE System
       TUnitary=.false.
       Tperiodicinmom=.false.
       t12FoldSym = .false.
+      t_initialized_roi = .false.
 
 #ifdef PROG_NUMRUNS_
       inum_runs = 1
