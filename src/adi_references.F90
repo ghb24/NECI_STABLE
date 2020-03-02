f7f910a9590381b177758b686de4f560a103e908
Kai Guther <k.guther@fkf.mpg.de>
2020-03-02 11:37:33 +0100

    explicitly use 0-indexed opt_source in semi_stoch_gen:generate_space_most_populated

--- src/adi_references.F90
+++ src/adi_references.F90
@@ -98,7 +98,7 @@ contains
          ! Get the nRefs most populated determinants
          refs_found = 0
          call generate_space_most_populated(maxNRefs, .false., 1, ref_buf, refs_found, &
-              CurrentDets, TotWalkers)
+              CurrentDets(0:NifTot,:), TotWalkers)
          ! Communicate the refs_found info
          mpi_refs_found = int(refs_found,MPIArg)
          call MPIAllGather(mpi_refs_found, refs_found_per_proc, ierr)
