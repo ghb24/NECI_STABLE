ce0904db873ef89b1a96c11984c2c8bf6a3a4ecb
Oskar Weser <oskar.weser@gmail.com>
2020-05-20 11:23:03 +0200

    Put correct compiler flags back in

--- cmake/compiler_flags/GNU_Fortran.cmake
+++ cmake/compiler_flags/GNU_Fortran.cmake
@@ -13,8 +13,7 @@ set( ${PROJECT_NAME}_Fortran_LINKER_FLAGS_CLUSTER "-flto" )
 # Warning flags ...
 set( ${PROJECT_NAME}_Fortran_WARNING_FLAGS "-Wall -Wextra -Wno-unused -Wno-zerotrip -Wno-maybe-uninitialized")
 # Treat errors as warnings
-# set( ${PROJECT_NAME}_Fortran_WARN_ERROR_FLAG "-Werror")
-set( ${PROJECT_NAME}_Fortran_WARN_ERROR_FLAG "")
+set( ${PROJECT_NAME}_Fortran_WARN_ERROR_FLAG "-Werror")
 
 # Treat 32bit/64bit compilation differently
 
