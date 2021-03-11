01ff345b9feec439d3d600f1f1ba47ac2016b026
Oskar Weser <oskar.weser@gmail.com>
2021-03-11 13:03:17 +0100

    Log the memory allocation with int64 was well (overloaded)
    * Now arrays > 2GB can be logged (Fortran has only signed ints)

--- src/lib/MemoryManagement.F90
+++ src/lib/MemoryManagement.F90
@@ -1,6 +1,6 @@
 #include "macros.h"
 module MemoryManager
-use constants , only : sizeof_int, dp, int64
+use constants , only : sizeof_int, dp, int64, int32
 
 ! JSS.  Memory book-keeping routines.  Contains a few elements of the initialisation,
 ! output and structure of the memory_manager module from CamCASP (formerly SITUS),
@@ -126,6 +126,27 @@ integer, save :: ismall=1 ! The smallest large object (remember to avoid repeati
 ! stored as the tag. Use long integer (int64) so can handle POINTER8.
 integer(int64), allocatable, save :: LookupPointer(:)
 
+! Log a memory allocation.
+! INPUT:
+!       ObjectName - Name of object.
+!       ObjectSize - Number of elements in object.
+!       ElementSize - Number of bytes per element.
+!       AllocRoutine - routine in which object is allocated.
+!       err (optional) - error output from allocate statement (checked if present).
+! OUTPUT:
+!       tag - position in memory log the object is stored at.
+!             If -1, then the log is full and it's not been stored.
+! IN/OUT:
+!       nCalls (optional) -  increments nCalls: counts the number of times
+!       a routine has called the LogMemAlloc routine (useful for tracking
+!       repeated allocations in debugging).
+!
+! Details:
+!       ObjectSize and ElementSize can either be both int32 or int64
+interface LogMemAlloc
+    module procedure LogMemAlloc_int32, LogMemAlloc_int64
+end interface
+
 contains
 
     subroutine InitMemoryManager(MemSize, print_err)
@@ -185,33 +206,29 @@ contains
     end subroutine InitMemoryManager
 
 
+    subroutine LogMemAlloc_int32(ObjectName,ObjectSize,ElementSize,AllocRoutine,tag,err,nCalls)
+        character(len=*), intent(in) :: ObjectName, AllocRoutine
+        integer(int32), intent(in) :: ObjectSize
+        integer(int32), intent(in) :: ElementSize
+        integer(TagIntType), intent(out) :: tag
+        integer, intent(in), optional :: err
+        integer, intent(inout), optional :: nCalls
+        call LogMemAlloc(ObjectName, int(ObjectSize, int64), int(ElementSize, int64), &
+                         AllocRoutine, tag, err, nCalls)
+    end subroutine
 
-    subroutine LogMemAlloc(ObjectName,ObjectSize,ElementSize,AllocRoutine,tag,err,nCalls)
-    ! Log a memory allocation.
-    ! INPUT:
-    !       ObjectName - Name of object.
-    !       ObjectSize - Number of elements in object.
-    !       ElementSize - Number of bytes per element.
-    !       AllocRoutine - routine in which object is allocated.
-    !       err (optional) - error output from allocate statement (checked if present).
-    ! OUTPUT:
-    !       tag - position in memory log the object is stored at.
-    !             If -1, then the log is full and it's not been stored.
-    ! IN/OUT:
-    !       nCalls (optional) -  increments nCalls: counts the number of times
-    !       a routine has called the LogMemAlloc routine (useful for tracking
-    !       repeated allocations in debugging).
-
+    subroutine LogMemAlloc_int64(ObjectName,ObjectSize,ElementSize,AllocRoutine,tag,err,nCalls)
     implicit none
 
-    character(len=*),intent(in) :: ObjectName,AllocRoutine
-    integer, intent(in) :: ObjectSize
-    integer, intent(in) :: ElementSize
+    character(len=*), intent(in) :: ObjectName, AllocRoutine
+    integer(int64), intent(in) :: ObjectSize
+    integer(int64), intent(in) :: ElementSize
     integer(TagIntType), intent(out) :: tag
     integer, intent(in), optional :: err
     integer, intent(inout), optional :: nCalls
 
-    integer :: ObjectSizeBytes,ismallloc(1)
+    integer(int64) :: ObjectSizeBytes
+    integer :: ismallloc(1)
     character(*), parameter :: this_routine = 'LogMemAlloc'
     external :: warning_neci
     if (present(nCalls)) nCalls=nCalls+1
@@ -250,7 +267,7 @@ contains
         tag=-1
         ! If we're not putting it in the log, test if it's a huge array:
         ! it's always the biggest fishes that get away!
-        if (ObjectSizeBytes.gt.LargeObjLog(ismall)%ObjectSize) then
+        if (ObjectSizeBytes > LargeObjLog(ismall)%ObjectSize) then
             LargeObjLog(ismall)%ObjectName=ObjectName
             LargeObjLog(ismall)%AllocRoutine=AllocRoutine
             LargeObjLog(ismall)%ObjectSize=ObjectSizeBytes
@@ -271,7 +288,7 @@ contains
     end if
 
     return
-    end subroutine LogMemAlloc
+    end subroutine LogMemAlloc_int64
 
 
 
