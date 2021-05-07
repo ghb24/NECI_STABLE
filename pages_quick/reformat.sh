9a2e13d0ff8150198c1b40dbfd223a18f5dacef3
Oskar Weser <oskar.weser@gmail.com>
2021-05-07 18:46:04 +0200

    Don't use uppercase LaTeX environments if not necessary

--- pages_quick/reformat.sh
+++ pages_quick/reformat.sh
@@ -8,14 +8,14 @@ cp $1 $tmpfile
 # replace html colour commands with latex colour commands
 
 sed -i 's/\[TOC\]/\\newpage/g' $tmpfile
-sed -i 's/@endnote/\\dummy\{\\End\{note\}\}/g' $tmpfile
-sed -i 's/@note/\\dummy\{\\Begin\{note\}\}/g' $tmpfile
-sed -i 's/@endwarning/\\dummy\{\\End\{warning\}\}/g' $tmpfile
-sed -i 's/@warning/\\dummy\{\\Begin\{warning\}\}/g' $tmpfile
-sed -i 's/@endtodo/\\dummy\{\\End\{todo\}\}/g' $tmpfile
-sed -i 's/@todo/\\dummy\{\\Begin\{todo\}\}/g' $tmpfile
-sed -i 's/@endbug/\\dummy\{\\End\{bug\}\}/g' $tmpfile
-sed -i 's/@bug/\\dummy\{\\Begin\{bug\}\}/g' $tmpfile
+sed -i 's/@endnote/\\dummy\{\\end\{note\}\}/g' $tmpfile
+sed -i 's/@note/\\dummy\{\\begin\{note\}\}/g' $tmpfile
+sed -i 's/@endwarning/\\dummy\{\\end\{warning\}\}/g' $tmpfile
+sed -i 's/@warning/\\dummy\{\\begin\{warning\}\}/g' $tmpfile
+sed -i 's/@endtodo/\\dummy\{\\end\{todo\}\}/g' $tmpfile
+sed -i 's/@todo/\\dummy\{\\begin\{todo\}\}/g' $tmpfile
+sed -i 's/@endbug/\\dummy\{\\end\{bug\}\}/g' $tmpfile
+sed -i 's/@bug/\\dummy\{\\begin\{bug\}\}/g' $tmpfile
 sed -i 's/<span style="color: \([^<]*\)">\([^<]*\)<\/span>/\\textcolor{\1}{\2}/g' $tmpfile
 
 # CONVERT
