99475756eaf0ad93df415ad48309099f05145f77
Oskar Weser <oskar.weser@gmail.com>
2021-05-07 10:46:58 +0200

    Added multilines in reformat.sh

--- pages_quick/reformat.sh
+++ pages_quick/reformat.sh
@@ -14,27 +14,27 @@ sed -i 's/\[TOC\]/\\newpage/g' $tmpfile
 # sed -i 's/@end\([^ ]*\)/\\dummy\{\\End\{\1\}\}/g' $tmpfile
 # sed -i 's/@\([^ ]*\)/\\dummy\{\\Begin\{\1\}\}/g' $tmpfile # important that end is first
 sed -i 's/@endnote/\\dummy\{\\End\{note\}\}/g' $tmpfile
-sed -i 's/@note/\\dummy\{\\Begin\{note\}\}/g' $tmpfile 
+sed -i 's/@note/\\dummy\{\\Begin\{note\}\}/g' $tmpfile
 sed -i 's/@endwarning/\\dummy\{\\End\{warning\}\}/g' $tmpfile
-sed -i 's/@warning/\\dummy\{\\Begin\{warning\}\}/g' $tmpfile 
+sed -i 's/@warning/\\dummy\{\\Begin\{warning\}\}/g' $tmpfile
 sed -i 's/@endtodo/\\dummy\{\\End\{todo\}\}/g' $tmpfile
-sed -i 's/@todo/\\dummy\{\\Begin\{todo\}\}/g' $tmpfile 
+sed -i 's/@todo/\\dummy\{\\Begin\{todo\}\}/g' $tmpfile
 sed -i 's/@endbug/\\dummy\{\\End\{bug\}\}/g' $tmpfile
-sed -i 's/@bug/\\dummy\{\\Begin\{bug\}\}/g' $tmpfile 
+sed -i 's/@bug/\\dummy\{\\Begin\{bug\}\}/g' $tmpfile
 sed -i 's/<span style="color: \([^<]*\)">\([^<]*\)<\/span>/\\textcolor{\1}{\2}/g' $tmpfile
 # sed -i 's/(@[a-z]*)+/\\begin\{$1\}/g' $tmpfile
 
 # CONVERT
 
 # pandoc "$1" \
-#        -f markdown-tex_math_dollars+tex_math_single_backslash \ 
+#        -f markdown-tex_math_dollars+tex_math_single_backslash \
 #        -V geometry:margin=3cm \
-#        -V geometry:a4paper \ 
+#        -V geometry:a4paper \
 #        -V fontsize=18pt \
 #        -V numbersections=true \
 #        --toc \
 #        -o "$2" \
-#        -t pdf 
+#        -t pdf
 
 # no idea why this works but the above doesn't (found online)
 # pandoc "$1" \
@@ -47,8 +47,15 @@ sed -i 's/<span style="color: \([^<]*\)">\([^<]*\)<\/span>/\\textcolor{\1}{\2}/g
 #     --pdf-engine=xelatex \
 #     -o "$2"
 
-# this works so clearly something is wrong in how I separate a multiline command
-# TODO make this multiline...
-pandoc -V geometry:margin=3cm -V geometry:a4paper -V fontsize=18pt -V numbersections=true --toc --pdf-engine=xelatex --include-in-header packages_to_include.tex "$tmpfile" -o "$2" -f markdown-tex_math_dollars+tex_math_single_backslash -t pdf 
+pandoc \
+      -V geometry:margin=3cm \
+      -V geometry:a4paper \
+      -V fontsize=18pt \
+      -V numbersections=true \
+      --toc \
+      --include-in-header packages_to_include.tex \
+      "$tmpfile" \
+      -o "$2" \
+      -f markdown-tex_math_dollars+tex_math_single_backslash
 
 rm $tmpfile
