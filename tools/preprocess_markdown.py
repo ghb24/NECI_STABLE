79cc483d721bca9a0bbbb204caf1b86aa5f33290
jphaupt <jphiliphaupt@gmail.com>
2021-06-14 17:12:39 -0700

    preprocessing script now also handles footnotes

--- tools/preprocess_markdown.py
+++ tools/preprocess_markdown.py
@@ -26,6 +26,7 @@ def ref_string(entry):
 
     return to_print.strip()
 
+# TODO the two functions below share a lot in common, probably good to modularise
 def replace_keys_get_ref(line, bib_database, to_end='', n=1, reffile=''):
     """
     given a string and a bibtexparser database object, gives back a parsed
@@ -40,11 +41,22 @@ def replace_keys_get_ref(line, bib_database, to_end='', n=1, reffile=''):
         if k in bib_database.entries_dict:
             if 'n' not in bib_database.entries_dict[k]:
                 bib_database.entries_dict[k].update({'n':n})
-                to_end += '\n\n<a id=\"%s\"></a>' % k + ref_string(bib_database.entries_dict[k])
+                to_end += '\n\n<a id=\"%s\"></a>[%i] ' % (k,n) + ref_string(bib_database.entries_dict[k])
                 n += 1
             line_parsed = line_parsed.replace('[@'+k+']', '<a href=\"%s#%s\">[%i]</a>' % (reffile,k,bib_database.entries_dict[k]['n']))
 
     return line_parsed, bib_database, to_end, n
+
+def replace_footnotes(line, n=1, to_end=''):
+    foot_regex = re.compile('footnote\{(.*?)\}')
+    footnotes_all = foot_regex.findall(line)
+    line_parsed = line
+    # n=1
+    for footnote in footnotes_all:
+        to_end += '\n\n<a id=\"%i\"></a><sup>%i</sup>' % (n,n) + footnote
+        line_parsed = line_parsed.replace('\\footnote{'+footnote+'}', '<a href=\"#%i\"><sup>%i</sup></a>' % (n,n))
+        n += 1
+    return line_parsed, to_end, n
 
 def preprocess_markdown_file(f, bib_database, reffile='', n=1, refs=''):
     processed_file = []
@@ -53,17 +65,24 @@ def preprocess_markdown_file(f, bib_database, reffile='', n=1, refs=''):
 
     # n = 1
     # refs = ''
+    nfoot = 1
+    to_end = ''
     line = f.readline()
     while line:
         if after_preamble:
             # processed_file.append(line)
             line_parsed, bib_database, refs, n = replace_keys_get_ref(line, bib_database, to_end=refs, n=n, reffile=reffile)
+            line_parsed, to_end, nfoot = replace_footnotes(line_parsed, n=nfoot, to_end=to_end)
         else:
             if line.strip() == '---':
                 after_preamble = True
             line_parsed = line
         processed_file.append(line_parsed)
         line = f.readline()
+
+    # add footnotes to end if applicable
+    if to_end:
+        processed_file.append('\n\n***'+to_end)
 
     return processed_file, bib_database, refs, n
 
