6c07b9ab6956367a5b4feca05403ae7ae2d7d1b5
Oskar Weser <oskar.weser@gmail.com>
2021-06-18 10:01:45 +0200

    prettified create_pdf_doc

--- tools/create_pdf_doc.py
+++ tools/create_pdf_doc.py
@@ -6,18 +6,11 @@ import re
 from pathlib import Path
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('input_file', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
-
-args = parser.parse_args()
-
-if not args.input_file:
-    sys.exit("Please provide an input file, or pipe it via stdin")
-
-
 ENVIRONMENTS = {'note', 'warning', 'todo', 'bug'}
-START_ENVIRONMENTS = {'@{}'.format(env): '\\verbatimLaTeX{{\\begin{{{}}}}}'.format(env) for env in ENVIRONMENTS}
-END_ENVIRONMENTS = {'@end{}'.format(env): '\\verbatimLaTeX{{\\end{{{}}}}}'.format(env) for env in ENVIRONMENTS}
+START_ENVIRONMENTS = {
+    '@{}'.format(env): '\\verbatimLaTeX{{\\begin{{{}}}}}'.format(env) for env in ENVIRONMENTS}
+END_ENVIRONMENTS = {
+    '@end{}'.format(env): '\\verbatimLaTeX{{\\end{{{}}}}}'.format(env) for env in ENVIRONMENTS}
 
 
 def process_markdown_file(f):
@@ -35,14 +28,14 @@ def process_markdown_file(f):
             processed_file.append('\\newpage\n')
             processed_file.append('# Using NECI\n')
 
-
         stripped = line.strip()
         if stripped in START_ENVIRONMENTS.keys():
             processed_file.append(START_ENVIRONMENTS[stripped] + '\n')
         elif stripped in END_ENVIRONMENTS.keys():
             processed_file.append(END_ENVIRONMENTS[stripped] + '\n')
         elif color_regex.search(line):
-            processed_file.append(color_regex.sub(r'\\textcolor{\1}{\2}', line))
+            processed_file.append(color_regex.sub(
+                r'\\textcolor{\1}{\2}', line))
         elif line == '---\n' and len(processed_file) >= 3:
             if 'title:' in processed_file[-1] and '---\n' == processed_file[-2]:
                 processed_file.pop()
@@ -56,7 +49,22 @@ def process_markdown_file(f):
         n += 1
     return processed_file
 
-processed_file = process_markdown_file(args.input_file)
 
-for line in processed_file:
-    print(line, end='')
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file', nargs='?',
+                        type=argparse.FileType('r'), default=sys.stdin)
+
+    args = parser.parse_args()
+
+    if not args.input_file:
+        sys.exit("Please provide an input file, or pipe it via stdin")
+
+    processed_file = process_markdown_file(args.input_file)
+
+    for line in processed_file:
+        print(line, end='')
+
+
+if __name__ == '__main__':
+    main()
