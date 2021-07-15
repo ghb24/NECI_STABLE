0d699f468f7e4e856b0c69290bbea8435fedf545
jphaupt <jphiliphaupt@gmail.com>
2021-07-15 14:17:20 -0700

    added flag to allow GNU fortran build with gcc 10+

--- cmake/compiler_flags/GNU_Fortran.cmake
+++ cmake/compiler_flags/GNU_Fortran.cmake
@@ -1,6 +1,6 @@
 # Special defines for gnu fortran compiler
 
-set( ${PROJECT_NAME}_Fortran_FLAGS "-g -ffree-line-length-none -fPIC" )
+set( ${PROJECT_NAME}_Fortran_FLAGS "-fallow-argument-mismatch -g -ffree-line-length-none -fPIC" )
 set( ${PROJECT_NAME}_Fortran_FLAGS_DEBUG "-O0 -fbounds-check -fcheck=all -fbacktrace -finit-real=nan -ffpe-trap=invalid,zero,overflow,underflow" )
 set( ${PROJECT_NAME}_Fortran_FLAGS_RELEASE "-O3 -march=native -mtune=native -funroll-loops" )
 set( ${PROJECT_NAME}_Fortran_FLAGS_CLUSTER "-flto" )
