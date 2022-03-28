df8b8981f64dc08008cb9e356addc3fba72d196d
Oskar Weser <oskar.weser@gmail.com>
2022-03-28 14:09:59 +0200

    Changed auto tools

--- tools/auto_cmake.sh
+++ tools/auto_cmake.sh
@@ -7,25 +7,18 @@
 
 echo "Calling cmake for: $@"
 
-if [ "gfortran-simple" == "$@" ]; then
-	cmake -DENABLE_HDF5=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_COMPILER=mpifort -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx ..
+if [ "gfortran" == "$@" ]; then
+	cmake -DENABLE_HDF5=ON -DENABLE_BUILD_HDF5=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_COMPILER=mpifort -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx ..
+elif [ "gfortran-debug" == "$@" ]; then
+	cmake -DENABLE_HDF5=ON -DENABLE_BUILD_HDF5=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_Fortran_COMPILER=mpifort -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx ..
 elif [ "gfortran-integer8" == "$@" ]; then
-	cmake -DENABLE_HDF5=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_FLAGS="-fdefault-integer-8" -DCMAKE_Fortran_COMPILER=mpifort -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx ..
-elif [ "fkf-ifort" == "$@" ] || [ "fkf-ifort-new" == "$@" ]; then
-	cmake -DENABLE_HDF5=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_COMPILER=mpiifort -DCMAKE_C_COMPILER=mpiicc -DCMAKE_CXX_COMPILER=mpiicpc ..
-elif [ "gfortran-simple -g" == "$@" ]; then
-	cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_HDF5=ON -DCMAKE_Fortran_COMPILER=mpifort -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx ..
-elif [ "fkf-ifort -g" == "$@" ]; then
+	cmake -DENABLE_HDF5=ON -DENABLE_BUILD_HDF5=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_FLAGS="-fdefault-integer-8" -DCMAKE_Fortran_COMPILER=mpifort -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx ..
+elif [ "ifort" == "$@" ] || [ "ifort-new" == "$@" ]; then
+	cmake -DENABLE_HDF5=ON -DENABLE_BUILD_HDF5=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_COMPILER=mpiifort -DCMAKE_C_COMPILER=mpiicc -DCMAKE_CXX_COMPILER=mpiicpc ..
+elif [ "ifort-debug" == "$@" ]; then
 	cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_HDF5=ON -DCMAKE_Fortran_COMPILER=mpiifort -DCMAKE_C_COMPILER=mpiicc -DCMAKE_CXX_COMPILER=mpiicpc ..
-elif [ "pgi-simple" == "$@" ]; then
-	cmake -DENABLE_HDF5=ON -DCMAKE_BUILD_TYPE=Release -DENABLE_SHARED_MEMORY=off ..
-elif [ "pgi-simple -g" == "$@" ]; then
-	cmake -DENABLE_HDF5=ON -DCMAKE_BUILD_TYPE=Debug -DENABLE_SHARED_MEMORY=off ..
-elif [ "fkf-ifort-hdf5" == "$@" ]; then
-	cmake -DENABLE_BUILD_HDF5=ON -DENABLE_HDF5=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_COMPILER=mpiifort -DCMAKE_C_COMPILER=mpiicc -DCMAKE_CXX_COMPILER=mpiicpc ..
-elif [ "gfortran-hdf5" == "$@" ]; then
+elif [ "gfortran-self_build_hdf5" == "$@" ]; then
 	cmake -DENABLE_BUILD_HDF5=ON -DENABLE_HDF5=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_COMPILER=mpifort -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx ..
 else
 	echo "Module not executed"
 fi
-
