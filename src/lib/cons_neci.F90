cda917813e7f68c77f886188e5c9182094b3ea12
Oskar Weser <oskar.weser@gmail.com>
2022-04-03 19:09:14 +0200

    trigger pipeline

--- src/lib/cons_neci.F90
+++ src/lib/cons_neci.F90
@@ -11,7 +11,6 @@ module constants
     public :: EPS
 
     ! Constant data.
-
     integer, parameter :: sp = selected_real_kind(6, 37)
     integer, parameter :: dp = selected_real_kind(15, 307)
     integer, parameter :: qp = selected_real_kind(33, 4931)
