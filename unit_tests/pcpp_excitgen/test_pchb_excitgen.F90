96ab9b21d8ab9206112b72335e06d00cf14b6806
Oskar Weser <oskar.weser@gmail.com>
2022-07-12 10:38:37 +0200

    Reworked PCHB FCI unit test

--- unit_tests/pcpp_excitgen/test_pchb_excitgen.F90
+++ unit_tests/pcpp_excitgen/test_pchb_excitgen.F90
@@ -1,15 +1,79 @@
+module test_pchb_excitgen_mod
+    use fruit
+    use constants, only: dp, maxExcit
+    use gasci_pchb, only: possible_PCHB_particle_selection
+    use pchb_excitgen, only: PCHB_FCI_excit_generator_t
+    use FciMCData, only: pSingles, pDoubles, pParallel
+    use SystemData, only: nEl
+    use sltcnd_mod, only: dyn_sltcnd_excit_old
+    use orb_idx_mod, only: calc_spin_raw, sum, SpinOrbIdx_t
+    use util_mod, only: near_zero
+    use unit_test_helper_excitgen, only: test_excitation_generator, &
+        init_excitgen_test, finalize_excitgen_test, generate_random_integrals, &
+        FciDumpWriter_t
+    use unit_test_helpers, only: run_excit_gen_tester
+    implicit none
+    private
+    public :: pchb_test_driver
+
+contains
+
+    subroutine pchb_test_driver()
+        integer, parameter :: n_iters = 5 * 10**7
+        type(PCHB_FCI_excit_generator_t) :: exc_generator
+        integer, parameter :: det_I(6) = [1, 2, 3, 7, 8, 10], n_spat_orb = 10
+        logical :: successful
+
+        pParallel = 0.05_dp
+        pSingles = 0.3_dp
+        pDoubles = 1.0_dp - pSingles
+
+        call init_excitgen_test(det_I, FciDumpWriter_t(random_fcidump, 'FCIDUMP'))
+
+        call exc_generator%init(possible_PCHB_particle_selection%UNIFORM)
+
+        call run_excit_gen_tester( &
+            exc_generator, 'PCHB FCI', &
+            opt_nI=det_I, opt_n_dets=n_iters, &
+            problem_filter=is_problematic,&
+            successful=successful)
+
+        call assert_true(successful)
+        call exc_generator%finalize()
+        call finalize_excitgen_test()
+
+
+    contains
+
+        subroutine random_fcidump(iunit)
+            integer, intent(in) :: iunit
+
+            call generate_random_integrals(&
+                iunit, n_el=size(det_I), n_spat_orb=n_spat_orb, &
+                sparse=0.7_dp, sparseT=0.7_dp, total_ms=sum(calc_spin_raw(det_I)))
+        end subroutine
+
+        logical function is_problematic(nI, exc, ic, pgen_diagnostic)
+            integer, intent(in) :: nI(nEl), exc(2, maxExcit), ic
+            real(dp), intent(in) :: pgen_diagnostic
+            is_problematic = &
+                (abs(1._dp - pgen_diagnostic) >= 0.1_dp) &
+                .and. .not. near_zero(dyn_sltcnd_excit_old(nI, ic, exc, .true.))
+        end function
+
+
+    end subroutine pchb_test_driver
+
+
+end module
+
+
 program test_pchb_excitgen
     use constants
     use fruit
     use Parallel_neci, only: MPIInit, MPIEnd
-    use pchb_excitgen, only: PCHB_FCI_excit_generator_t
-    use unit_test_helper_excitgen
-    use orb_idx_mod, only: beta
-    use procedure_pointers, only: generate_excitation
-    use gasci_pchb, only: possible_PCHB_particle_selection
+    use test_pchb_excitgen_mod, only: pchb_test_driver
     implicit none
-
-    type(PCHB_FCI_excit_generator_t) :: PCHB_FCI
 
     call MPIInit(.false.)
     call init_fruit()
@@ -23,83 +87,4 @@ program test_pchb_excitgen
     end block
     call MPIEnd(.false.)
 
-contains
-
-    subroutine pchb_test_driver()
-        implicit none
-        real(dp) :: pTot, pNull
-        integer :: numEx, nFound, i
-        ! There can be some excitations with really low matrix elements -> we need a lot
-        ! of samples to hit all
-        integer, parameter :: nSamples = 1000000
-
-        ! set the excitation generator to pchb
-        generate_excitation => gen_rand_excit_pchb
-        calc_pgen => calc_pgen_pchb
-
-        ! prepare an excitation generator test
-        call init_excitgen_test(ref_det=[(i, i=1, 5)], fcidump_writer=FciDumpWriter_t(random_fcidump, 'FCIDUMP'))
-
-        ! prepare the pchb excitgen: set the weights/map-table
-        call set_ref()
-        call PCHB_FCI%init(possible_PCHB_particle_selection%UNIFORM)
-
-        ! test the excitation generator
-        call test_excitation_generator(nSamples, pTot, pNull, numEx, nFound, .true.)
-        ! make sure all excits have been found
-        call assert_equals(numEx, nFound)
-        ! and the total prob is 1.0
-        call assert_true(abs(1.0 - pTot) < 0.05)
-
-        ! free memory
-        call free_ref()
-        call PCHB_FCI%finalize()
-    end subroutine pchb_test_driver
-
-    function calc_pgen_pchb(nI, ilutI, ex, ic, ClassCount2, ClassCountUnocc2) result(pgen)
-        use constants
-        use SymExcitDataMod, only: scratchSize
-        use bit_rep_data, only: NIfTot
-        use SystemData, only: nel
-        implicit none
-        integer, intent(in) :: nI(nel)
-        integer(n_int), intent(in) :: ilutI(0:NIfTot)
-        integer, intent(in) :: ex(2, 2), ic
-        integer, intent(in) :: ClassCount2(ScratchSize), ClassCountUnocc2(ScratchSize)
-
-        real(dp) :: pgen
-        integer :: help_ex(2, maxExcit)
-        help_ex(:, :2) = ex
-
-        pgen = PCHB_FCI%get_pgen(nI, ilutI, help_ex, ic, ClassCount2, ClassCountUnocc2)
-    end function
-
-    subroutine gen_rand_excit_pchb(nI, ilutI, nJ, ilutJ, exFlag, ic, &
-                                   ex, tParity, pGen, hel, store, part_type)
-
-        use SystemData, only: nel
-        use bit_rep_data, only: NIfTot
-        use FciMCData, only: excit_gen_store_type
-        use constants
-        implicit none
-
-        integer, intent(in) :: nI(nel), exFlag
-        integer(n_int), intent(in) :: ilutI(0:NIfTot)
-        integer, intent(out) :: nJ(nel), ic, ex(2, maxExcit)
-        integer(n_int), intent(out) :: ilutJ(0:NifTot)
-        real(dp), intent(out) :: pGen
-        logical, intent(out) :: tParity
-        HElement_t(dp), intent(out) :: hel
-        type(excit_gen_store_type), intent(inout), target :: store
-        integer, intent(in), optional :: part_type
-
-        call PCHB_FCI%gen_exc(nI, ilutI, nJ, ilutJ, exFlag, ic, &
-                              ex, tParity, pGen, hel, store, part_type)
-    end subroutine
-
-    subroutine random_fcidump(iunit)
-        integer, intent(in) :: iunit
-        call generate_random_integrals( &
-            iunit, n_el=5, n_spat_orb=12, sparse=0.9_dp, sparseT=0.1_dp, total_ms=beta)
-    end subroutine
 end program test_pchb_excitgen
