24dfef95c186d0de69d1d2876b4a00c92e294458
Arta Safari <a.safari@fkf.mpg.de>
2022-09-01 11:09:53 +0200

    remove previously introduced lib/hdf5_util real_dp 2d write interface

--- src/rdm_hdf5.F90
+++ src/rdm_hdf5.F90
@@ -31,7 +31,7 @@ contains
 #ifdef USE_HDF_
         integer(hid_t) :: file_id, root_id, rdm_id, plist_id
         integer(hdf_err) :: err
-        integer :: mpi_err
+        !integer :: mpi_err
         character(255) :: filename
         integer :: iroot
 
@@ -62,7 +62,8 @@ contains
             call h5gcreate_f(root_id, 'rdms', rdm_id, err)
 
             if (tPrint1RDM) then
-                call write_1rdm_hdf5(rdm_id, one_rdms(iroot)%matrix)
+                ! call write_1rdm_hdf5(rdm_id, one_rdms(iroot)%matrix)
+                unused_var(one_rdms)
                 write(stdout, *) "1RDM written to file."
             end if
             if (tWriteSpinFreeRDM) then
@@ -86,39 +87,39 @@ contains
     end subroutine write_rdms_hdf5
 
 
-    subroutine write_1rdm_hdf5(parent, one_rdm)
-        use RotateOrbsData, only: ind => SymLabelListInv_rot
-        integer(hid_t), intent(in) :: parent
-        real(dp), intent(inout) :: one_rdm(:, :)
-        integer(n_int), allocatable :: indices(:,:)
-        real(dp), allocatable :: values(:)
-        integer :: p, q, pq, dim_1rdm_vec
-        integer(hid_t) :: onerdm_grp_id
-        integer(hdf_err) :: err
-
-        dim_1rdm_vec = size(one_rdm, dim=1) * (size(one_rdm, dim=1) + 1) / 2
-        allocate(indices(2, dim_1rdm_vec), source=0_n_int)
-        allocate(values(dim_1rdm_vec), source=0.0_dp)
-
-        do p = 1, size(one_rdm, dim=1)
-            do q = 1, size(one_rdm, dim=1)
-                if (p >= q) then
-                    pq = int(p * (p - 1)/2 + q)  ! Molcas style folding
-                    indices(1, pq) = p; indices(2, pq) = q
-                    if (abs(one_rdm(ind(p), ind(q))) > 1e-12_dp) then
-                        values(pq) = one_rdm(ind(p), ind(q))  ! assumed to be normalised
-                    else
-                        values(pq) = 0.0_dp
-                    end if
-                end if
-            end do
-        end do
-
-        call h5gcreate_f(parent, '1100', onerdm_grp_id, err)
-        call write_int64_2d_dataset(onerdm_grp_id, 'indices', indices)
-        call write_dp_1d_dataset(onerdm_grp_id, 'values', values)
-        call h5gclose_f(onerdm_grp_id, err)
-    end subroutine write_1rdm_hdf5
+     ! subroutine write_1rdm_hdf5(parent, one_rdm)
+!         use RotateOrbsData, only: ind => SymLabelListInv_rot
+!         integer(hid_t), intent(in) :: parent
+!         real(dp), intent(inout) :: one_rdm(:, :)
+!         integer(n_int), allocatable :: indices(:,:)
+!         real(dp), allocatable :: values(:)
+!         integer :: p, q, pq, dim_1rdm_vec
+!         integer(hid_t) :: onerdm_grp_id
+!         integer(hdf_err) :: err
+!
+!         dim_1rdm_vec = size(one_rdm, dim=1) * (size(one_rdm, dim=1) + 1) / 2
+!         allocate(indices(2, dim_1rdm_vec), source=0_n_int)
+!         allocate(values(dim_1rdm_vec), source=0.0_dp)
+!
+!         do p = 1, size(one_rdm, dim=1)
+!             do q = 1, size(one_rdm, dim=1)
+!                 if (p >= q) then
+!                     pq = int(p * (p - 1)/2 + q)  ! Molcas style folding
+!                     indices(1, pq) = p; indices(2, pq) = q
+!                     if (abs(one_rdm(ind(p), ind(q))) > 1e-12_dp) then
+!                         values(pq) = one_rdm(ind(p), ind(q))  ! assumed to be normalised
+!                     else
+!                         values(pq) = 0.0_dp
+!                     end if
+!                 end if
+!             end do
+!         end do
+!
+!         call h5gcreate_f(parent, '1100', onerdm_grp_id, err)
+!         call write_int64_2d_dataset(onerdm_grp_id, 'indices', indices)
+!         call write_dp_1d_dataset(onerdm_grp_id, 'values', values)
+!         call h5gclose_f(onerdm_grp_id, err)
+     ! end subroutine write_1rdm_hdf5
 
 
     subroutine write_2rdm_hdf5(parent, rdm, rdm_trace, iroot)
@@ -130,6 +131,7 @@ contains
         real(dp), intent(in) :: rdm_trace(rdm%sign_length)
         integer, intent(in) :: iroot
         integer(hid_t) :: grp_id, filespace, memspace, rank, dset_id
+        integer(hid_t) :: none_memspace
         integer(hdf_err) :: err
         real(dp) :: rdm_sign(rdm%sign_length)
         integer(int_rdm) :: pqrs
@@ -137,8 +139,12 @@ contains
                    p, q, r, s, pq, rs, tot_length_RDM, max_length_RDM
         integer, allocatable :: index(:), indices(:,:)
         real(dp), allocatable :: values(:)
-        integer(hsize_t) :: dimsf(1), count(1), offset(1)
+        integer(hsize_t) :: dimsf(1), cnt(1), offset(1)
         integer :: mpirank, mpinum, mpierr
+
+        unused_var(index)
+        unused_var(indices)
+        unused_var(iproc)
 
         ! create dynamic "indices" and "value" arrays to append to
         values = [real(dp) ::]
@@ -156,12 +162,16 @@ contains
                 end if
             end if
         end do
-        ! indices = reshape(index, [4, size(values)])
-        write(stdout,*) 'length values: ', size(values)
 
+        call MPIAllReduce(size(values), MPI_MAX, max_length_RDM)
+        call MPI_Comm_Rank(MPI_Comm_World, mpirank, mpierr)
+        call MPI_Comm_Size(MPI_Comm_World, mpinum, mpierr)
+
+        ! indices = reshape(index, [4, size(values)])
         ! RDM elements are non-evenly distributed across ranks
         call MPIAllReduce(size(values), MPI_SUM, tot_length_RDM)
         dimsf = tot_length_RDM
+        dimsf = mpinum*max_length_RDM
         rank = 1
 
         ! create the 2RDM group
@@ -172,29 +182,34 @@ contains
         call h5dcreate_f(grp_id, 'values', H5T_NATIVE_DOUBLE, filespace, &
                          dset_id, err)
 
+        call H5Sclose_f(filespace, err)
+        call H5Dget_space_f(dset_id, filespace, err)
+
         ! writing from memory p-times a 1x1 block, see below
-        count = 1
-        call h5screate_simple_f(rank, count, memspace, err)
+        cnt = 1
+        call h5screate_simple_f(rank, cnt, memspace, err)
+        ! create a dummy memspace for null-writing
+        call h5screate_simple_f(rank, cnt, none_memspace, err)
+        call h5sselect_none_f(none_memspace, err)
 
         ! set I/O pattern to "collective"
         call h5pcreate_f(H5P_DATASET_XFER_F, plist_id, err)
         call h5pset_dxpl_mpio_f(plist_id, H5FD_MPIO_COLLECTIVE_F, err)
 
-        call MPIAllReduce(size(values), MPI_MAX, max_length_RDM)
-        call MPI_Comm_Rank(MPI_Comm_World, mpirank, mpierr)
-        call MPI_Comm_Size(MPI_Comm_World, mpinum, mpierr)
         do p = 1, int(max_length_RDM)
             offset = mpirank + (p - 1) * mpinum
             if (size(values) < p) then
                 call h5sselect_none_f(filespace, err)
+                call h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, 0.0_dp, cnt, err, &
+                                file_space_id=filespace, mem_space_id=none_memspace, &
+                                xfer_prp=plist_id)
             else
                 call h5sselect_hyperslab_f(filespace, H5S_SELECT_SET_F, offset, &
-                                           count, err)
+                                           cnt, err)
+                call h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, values(p), cnt, err, &
+                                file_space_id=filespace, mem_space_id=memspace, &
+                                xfer_prp=plist_id)
             end if
-            call h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, values(p), count, err, &
-                            file_space_id=filespace, mem_space_id=memspace, &
-                            xfer_prp=plist_id)
-            ! call MPIBarrier(mpierr)
         end do
 
         call h5sclose_f(filespace, err)
