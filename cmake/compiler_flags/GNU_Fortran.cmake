c3a62830fa80d434fcbe7530fec0c0cd9c98c54e
jphaupt <jphaupt@protonmail.com>
2023-01-17 09:20:51 +0100

    Remove -Wno-do-subscript to avoid warnings

--- cmake/compiler_flags/GNU_Fortran.cmake
+++ cmake/compiler_flags/GNU_Fortran.cmake
@@ -14,7 +14,7 @@ set( ${PROJECT_NAME}_Fortran_LINKER_FLAGS_FASTDEBUG "-rdynamic" )
 set( ${PROJECT_NAME}_Fortran_LINKER_FLAGS_CLUSTER "-flto" )
 
 # Warning flags. They are only set when warnings are enabled.
-set( ${PROJECT_NAME}_Fortran_WARNING_FLAGS "-Wall -Wextra -Wno-zerotrip -Wno-maybe-uninitialized -Wno-do-subscript -Wcharacter-truncation -Wuse-without-only -Wimplicit-procedure -Winteger-division -Wreal-q-constant ")
+set( ${PROJECT_NAME}_Fortran_WARNING_FLAGS "-Wall -Wextra -Wno-zerotrip -Wno-maybe-uninitialized -Wcharacter-truncation -Wuse-without-only -Wimplicit-procedure -Winteger-division -Wreal-q-constant ")
 # Disable some warnings for legacy code. **These flags are always enabled for the respective files.**.
 set( ${PROJECT_NAME}_Fortran_relaxed_WARNING_FLAGS "-Wno-unused -Wno-character-truncation -Wno-use-without-only -Wno-implicit-procedure -Wno-integer-division -Wno-real-q-constant ")
 if( CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 10 )
