4c83ff41d9396b5b9c8b69492f53cf0345855946
Oskar Weser <oskar.weser@gmail.com>
2023-03-05 15:43:15 +0100

    small docstring to blas_interface module

--- src/blas_interface.F90
+++ src/blas_interface.F90
@@ -1,6 +1,10 @@
 #include "macros.h"
 
 module blas_interface_mod
+    !! This module should contain the declaration of BLAS routines.
+    !!
+    !! At the moment they are just declared as external,
+    !! but one can change that one by one.
     better_implicit_none
 
     private
