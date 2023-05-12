7d7b86748083ec4f57cb0da52381b8ac01b29b9f
Oskar Weser <oskar.weser@gmail.com>
2023-05-12 11:43:25 +0200

    Added unit test for decide on PCHB

--- unit_tests/gasci/test_gasci_pchb_rhf_hermitian.F90
+++ unit_tests/gasci/test_gasci_pchb_rhf_hermitian.F90
@@ -31,6 +31,7 @@ contains
     subroutine test_gasci_driver()
         call run_test_case(test_pgen_RHF_hermitian, "test_pgen_RHF_hermitian")
         call run_test_case(test_string_conversion, "test_string_conversion")
+        call run_test_case(test_decide_on_option, "test_decide_on_option")
     end subroutine test_gasci_driver
 
     subroutine test_string_conversion()
@@ -49,6 +50,62 @@ contains
         options = doubles_options_vals%from_str("FULL-FULL:FAST-FAST")
         call assert_true(options%particle_selection == doubles_options_vals%particle_selection%FULL_FULL)
         call assert_true(options%hole_selection == doubles_options_vals%hole_selection%FAST_FAST)
+    end subroutine
+
+    subroutine test_decide_on_option()
+        use gasci_pchb_main, only: &
+            GAS_PCHB_OptionsUserInput_t, GAS_PCHB_user_input_vals, &
+            GAS_PCHB_options_t, vals => GAS_PCHB_options_vals, decide_on_PCHB_options
+
+        type(GAS_PCHB_options_t) :: options
+
+        options = decide_on_PCHB_options(&
+            GAS_PCHB_OptionsUserInput_t(&
+                GAS_PCHB_user_input_vals%option_selection%LOCALISED), &
+            loc_nBasis=20, loc_nEl=20, loc_tUHF=.false.)
+
+        call assert_true(options%doubles%spin_orb_resolved)
+        call assert_true(options%singles%algorithm == vals%singles%algorithm%PC_weighted)
+        call assert_true(options%singles%PC_weighted%drawing == vals%singles%PC_weighted%drawing%UNIF_FULL)
+        call assert_true(options%doubles%particle_selection == vals%doubles%particle_selection%UNIF_FULL)
+        call assert_true(options%doubles%hole_selection == vals%doubles%hole_selection%FULL_FULL)
+
+
+        options = decide_on_PCHB_options(&
+            GAS_PCHB_OptionsUserInput_t(&
+                GAS_PCHB_user_input_vals%option_selection%LOCALISED), &
+            loc_nBasis=100, loc_nEl=20, loc_tUHF=.false.)
+
+        call assert_true(.not. options%doubles%spin_orb_resolved)
+        call assert_true(options%singles%algorithm == vals%singles%algorithm%PC_weighted)
+        call assert_true(options%singles%PC_weighted%drawing == vals%singles%PC_weighted%drawing%UNIF_FAST)
+        call assert_true(options%doubles%particle_selection == vals%doubles%particle_selection%UNIF_FULL)
+        call assert_true(options%doubles%hole_selection == vals%doubles%hole_selection%FAST_FAST)
+
+
+        options = decide_on_PCHB_options(&
+            GAS_PCHB_OptionsUserInput_t(&
+                GAS_PCHB_user_input_vals%option_selection%DELOCALISED), &
+            loc_nBasis=20, loc_nEl=20, loc_tUHF=.false.)
+
+        call assert_true(options%doubles%spin_orb_resolved)
+        call assert_true(options%singles%algorithm == vals%singles%algorithm%BITMASK_UNIFORM)
+        call assert_true(options%singles%PC_weighted%drawing == vals%singles%PC_weighted%drawing%UNDEFINED)
+        call assert_true(options%doubles%particle_selection == vals%doubles%particle_selection%UNIF_UNIF)
+        call assert_true(options%doubles%hole_selection == vals%doubles%hole_selection%FULL_FULL)
+
+
+        options = decide_on_PCHB_options(&
+            GAS_PCHB_OptionsUserInput_t(&
+                GAS_PCHB_user_input_vals%option_selection%DELOCALISED), &
+            loc_nBasis=100, loc_nEl=20, loc_tUHF=.false.)
+
+        call assert_true(.not. options%doubles%spin_orb_resolved)
+        call assert_true(options%singles%algorithm == vals%singles%algorithm%BITMASK_UNIFORM)
+        call assert_true(options%singles%PC_weighted%drawing == vals%singles%PC_weighted%drawing%UNDEFINED)
+        call assert_true(options%doubles%particle_selection == vals%doubles%particle_selection%UNIF_UNIF)
+        call assert_true(options%doubles%hole_selection == vals%doubles%hole_selection%FAST_FAST)
 
     end subroutine
+
 end program test_gasci_program
